5196b3c4accaeca56e858b99f3967e194e5cef97
phil@home <philipp.millner@outlook.com>
2024-03-03 12:05:44 +0100

    Dropped MongoDB - Changed to Postgresql
    Dropped the MongoDB for PostgreSQL hosted on Neon.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,24 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
-    ThrottlerModule.forRoot([
-      {
-        name: 'default',
-        ttl: 60,
-        limit: 10,
-      },
-      {
-        name: 'verificationEmail',
-        ttl: 60 * 60, // 1 hour
-        limit: 3,
-      },
-    ]),
   ],
 })
 export class AppModule {}
