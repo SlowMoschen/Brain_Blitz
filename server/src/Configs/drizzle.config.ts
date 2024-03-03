f0ce5935d5a28f4665cfa433535927e76d5a6478
phil@home <philipp.millner@outlook.com>
2024-03-03 16:39:06 +0100

    Drizzle Setup
    Setup for drizzle done - Setup for Postgresql Sessions done

---
+++ server/src/Configs/drizzle.config.ts
@@ -0,0 +1,10 @@
+import { Config } from "drizzle-kit";
+
+export default {
+    schema: "./src/DB/schema/**/*.ts",
+    out: "./src/DB/migrations",
+    driver: "pg",
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL,
+    }
+} satisfies Config
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,33 +4,33 @@ import helmet from 'helmet';
 import loggerMiddleware from './Middlewares/Logger.Middleware';
 import * as session from 'express-session';
 import * as passport from 'passport';
-import * as connectMongo from 'connect-mongodb-session';
-const MongoDBStore = connectMongo(session);
+import * as connectPGSession from 'connect-pg-simple';
+const pgSession = connectPGSession(session);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: {
-    origin: '*',
-    credentials: true,
-  } });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      credentials: true,
+    },
+  });
 
   app.use(helmet());
   app.use(loggerMiddleware);
 
   app.use(
     session({
+      store: new pgSession({
+        conString: process.env.DATABASE_URL,
+        tableName: 'user_sessions',
+        createTableIfMissing: true,
+      }),
       secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
-      cookie: {
-        maxAge: process.env.SESSION_MAX_AGE,
-      },
-      store: new MongoDBStore({
-        uri: process.env.MONGO_URI,
-        collection: 'sessions',
-        expires: process.env.SESSION_MAX_AGE,
-      }),
-    })
-  )
+      cookie: parseInt(process.env.SESSION_MAX_AGE),
+    }),
+  );
 
   app.use(passport.initialize());
   app.use(passport.session());
