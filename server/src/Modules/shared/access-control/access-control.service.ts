9bcd415944e736451c6369dcc9a378b444dae6e8
phil@home <philipp.millner@outlook.com>
2024-03-05 15:09:58 +0100

    [new] Added a Access Control Service
    Service for generating Role Hierarchy and check if user is Authorized

---
+++ server/src/Modules/shared/access-control/access-control.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Role } from 'src/Enums/role.enum';
+
+interface isAuthorizedParams {
+    userRole: Role;
+    requiredRole: Role;
+}
+
+@Injectable()
+export class AccessControlService {
+    private hierachies: Array<Map<string, number>> = [];
+    private priority: number = 1;
+
+    cpnstructor() {
+        this.buildHierachies([
+            Role.USER,
+            Role.ADMIN,
+        ]);
+    }
+
+    private buildHierachies(roles: Role[]) {
+        const hierarchy: Map<string, number> = new Map()
+        roles.forEach((role) => {
+            hierarchy.set(role, this.priority);
+            this.priority++;
+        });
+        this.hierachies.push(hierarchy);
+    }
+
+    public isAuthorized({ userRole, requiredRole }: isAuthorizedParams) {
+        for (const hierarchy of this.hierachies) {
+            const prio = hierarchy.get(userRole);
+            const requiredPrio = hierarchy.get(requiredRole);
+
+            if (prio && requiredPrio && prio >= requiredPrio) return true;
+        }
+        return false;
+    }
+}
