c5fbc090f1cd7f2e528fb1be07885acb7da8a504
phil@home <philipp.millner@outlook.com>
2024-03-05 13:28:21 +0100

    [refac] Refactored User Cred DTO
    every field is now required to be send because we it is a PUT request

--- server/src/Modules/users/dto/update-user-credentials.dto.ts
+++ server/src/Modules/users/dto/update-user-credentials.dto.ts
@@ -1,22 +1,18 @@
 import { IsOptional, IsString, MinLength, IsEmail, Matches } from "class-validator";
 
 export class UpdateUserCredentialsDTO {
-    @IsOptional()
     @IsString()
     @MinLength(2, { message: 'First name is too short' })
     first_name: string;
 
-    @IsOptional()
     @IsString()
     @MinLength(2, { message: 'Last name is too short' })
     last_name: string;
 
-    @IsOptional()
     @IsString()
     @IsEmail()
     email: string;
 
-    @IsOptional()
     @IsString()
     @MinLength(8, { message: 'Password is too short' })
     @Matches(/[^a-zA-Z0-9]/, { message: 'Password must contain at least one special character' })
