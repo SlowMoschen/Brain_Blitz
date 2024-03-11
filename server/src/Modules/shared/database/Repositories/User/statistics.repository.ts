e6942c5f1322cfceb38d1d200dff6894769d633e
phil@home <philipp.millner@outlook.com>
2024-03-11 09:33:20 +0100

    StatisticsRepo implemented

---
+++ server/src/Modules/shared/database/Repositories/User/statistics.repository.ts
@@ -0,0 +1,57 @@
+import { Injectable } from "@nestjs/common";
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
+import { InjectDatabase } from "src/Decorators/injectDatabase.decorator";
+import * as schema from '../../../../../Models/_index';
+import { SelectUserStatistics } from "src/Utils/Types/model.types";
+import { eq } from "drizzle-orm";
+
+@Injectable()
+export class StatisticsRepository {
+    constructor(@InjectDatabase() private readonly db: NodePgDatabase<typeof schema>) {}
+
+    /**
+     * @description - Queries the database for all statistics
+     * @returns {Promise<SelectUserStatistics[] | [] | Error>} - Returns all statistics or an empty array if no statistics are found and an error if an error occurs
+     */
+    async queryAllStatistics(): Promise<SelectUserStatistics[] | [] | Error> {
+        try {
+            return await this.db.select().from(schema.usersStatisticsTable);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /**
+     * @description - Queries the database for a statistics by id
+     * @param id - The id of the statistics
+     * @returns {Promise<SelectUserStatistics | [] | Error>} - Returns a statistics or an empty array if no statistics are found and an error if an error occurs
+     */
+    async queryStatisticsById(id: string): Promise<SelectUserStatistics | [] | Error> {
+        try {
+            return await this.db.query.usersStatisticsTable.findFirst({
+                where: eq(schema.usersStatisticsTable.user_id, id)
+            });
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /**
+     * @description - Updates a statistics by id
+     * @param id - The id of the statistics
+     * @param body - The body of the request
+     * @returns {Promise<string | [] | Error>} - Returns the userID or an empty array if no statistics are found and an error if an error occurs
+     */
+    async updateStatistics(id: string, body: any): Promise<string | [] | Error> {
+        try {
+            const statistics = await this.db
+                .update(schema.usersStatisticsTable)
+                .set(body)
+                .where(eq(schema.usersStatisticsTable.user_id, id))
+                .returning({ user_id: schema.usersStatisticsTable.user_id });
+            return statistics ? statistics[0].user_id : [];
+        } catch (error) {
+            return error;
+        }
+    }
+}
