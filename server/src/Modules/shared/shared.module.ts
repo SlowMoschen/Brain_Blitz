19f5a89b6dba550b9db78963cae1a8e36047594d
phil@home <philipp.millner@outlook.com>
2024-03-14 16:48:42 +0100

    [misc] removed ResponseHelperService from shared module

--- server/src/Modules/shared/shared.module.ts
+++ server/src/Modules/shared/shared.module.ts
@@ -2,12 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { AccessControlService } from './access-control/access-control.service';
 import { DatabaseModule } from './database/db.module';
 import { EncryptionModule } from './encryption/encryption.module';
-import { ResponseHelperService } from './responseHelper.service';
 
 @Global()
 @Module({
 	imports: [DatabaseModule, EncryptionModule],
-	providers: [ResponseHelperService, AccessControlService],
-	exports: [AccessControlService, ResponseHelperService, EncryptionModule, DatabaseModule],
+	providers: [AccessControlService],
+	exports: [AccessControlService, EncryptionModule, DatabaseModule],
 })
 export class SharedModule {}
