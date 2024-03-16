242159c19ca5bddfa85fa3defb99e166e14a27eb
phil@home <philipp.millner@outlook.com>
2024-03-16 23:56:07 +0100

    [misc] removed Helper Function
    Removed extractKeysFromTable Function because it wasnt used and was replaced by filteredTypes

--- server/src/Enums/timestampColumns.enum.ts
+++ server/src/Enums/timestampColumns.enum.ts
@@ -1,6 +1,4 @@
 import { FilteredKeys } from 'src/Utils/Types/enum.types';
 import { usersTimestampsTable } from '../Models/_index';
-import { extractKeysFromTable } from 'src/Utils/Helpers/extractKeysFromTable';
 
-const timestampsArray = [...extractKeysFromTable(usersTimestampsTable)] as const;
 export type TimestampColumns = FilteredKeys<typeof usersTimestampsTable._.columns>;
--- server/src/Utils/Helpers/extractKeysFromTable.ts
+++
@@ -1,8 +0,0 @@
-import { PgTableWithColumns } from "drizzle-orm/pg-core"
-
-// TODO: remove any Type
-export const extractKeysFromTable = (table: PgTableWithColumns<any>) => {
-    return Object.keys(table)
-		.filter((column) => column !== 'id' && column !== 'user_id')
-		.map((column) => column.toUpperCase())
-}
