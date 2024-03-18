e99a566f1cfa87feb4c83604e5cb151b5a5b85c6
phil@home <philipp.millner@outlook.com>
2024-03-18 09:53:23 +0100

    [new] Added EventModule to Decouple services event bases

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,12 @@ import { AuthModule } from './Modules/auth/auth.module';
 import { QuizzesModule } from './Modules/quizzes/quizzes.module';
 import { SharedModule } from './Modules/shared/shared.module';
 import { UsersModule } from './Modules/users/users.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    EventEmitterModule.forRoot(),
     SharedModule,
     AuthModule,
     UsersModule,
