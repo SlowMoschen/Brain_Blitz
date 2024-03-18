6ee7c68935c4e652e45b2799d513ed28ed84585d
phil@home <philipp.millner@outlook.com>
2024-03-18 14:22:47 +0100

    [misc] removed db:clear script
    Added the clearing of the DB into the seeding script. Seeding script now inserts 3 Test quizzes

--- server/src/DB/clear.ts
+++ server/src/DB/clear.ts
@@ -1,33 +1,15 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Client } from 'pg';
-import { env } from '../Configs/env.config';
-import * as schema from '../Models/_index';
 import * as tables from '../Models/_tables';
 
-const DB_URL =
-  env.NODE_ENV === 'production' ? env.DATABASE_URL : env.DATABASE_DEV_URL;
-
-const client = new Client({
-  connectionString: DB_URL,
-});
-
-const db = drizzle(client, { schema });
-
-const clearDB = async () => {
+export const clearDB = async (dbConnection) => {
   try {
-    await client.connect();
-    console.log('Connected to database');
     const reveresedTables = Object.keys(tables).reverse();
     
     for (const table of reveresedTables) {
-      await db.delete(tables[table]);
+      await dbConnection.delete(tables[table]);
     }
     
     console.log('Database cleared');
   } catch (err) {
     console.error(err);
-  } finally {
-    await client.end();
   }
 };
-clearDB();
--- server/src/DB/seed.ts
+++ server/src/DB/seed.ts
@@ -1,16 +1,9 @@
-import { eq } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Client } from 'pg';
 import { InsertQuiz } from 'src/Utils/Types/model.types';
 import { env } from '../Configs/env.config';
 import * as schema from '../Models/_index';
-import {
-	usersBillingInformationTable,
-	usersSettingsTable,
-	usersStatisticsTable,
-	usersTable,
-	usersTimestampsTable
-} from '../Models/_index';
+import { clearDB } from './clear';
 
 const DB_URL = env.NODE_ENV === 'production' ? env.DATABASE_URL : env.DATABASE_DEV_URL;
 
@@ -19,27 +12,6 @@ const client = new Client({
 });
 
 const db = drizzle(client, { schema });
-
-const usersToInsert = [
-	{
-		first_name: 'John',
-		last_name: 'Doe',
-		email: 'test@mail.com',
-		password: 'password',
-	},
-	{
-		first_name: 'Jane',
-		last_name: 'Doe',
-		email: 'test1@mail.com',
-		password: 'password',
-	},
-	{
-		first_name: 'John',
-		last_name: 'Smith',
-		email: 'test2@mail.com',
-		password: 'password',
-	},
-];
 
 const quizzesToInsert: InsertQuiz[] = [
 	{
@@ -79,122 +51,27 @@ const seedQuizzes = async () => {
 					answers: ['1', '2', '3', '4'],
 					correct_answer: '2',
 				}),
-			]);
-		}
-	} catch (err) {
-		console.error(err);
-	}
-};
-
-const seedQuizWithHighscores = async () => {
-	try {
-		console.log('Seeding quiz with highscores');
-
-		const quiz = await db.query.quizzesTable.findFirst();
-		const user = await db.query.usersTable.findFirst();
-
-		if (quiz && user) {
-			const highscore = await db.insert(schema.quizHighscoresTable).values({
-				quiz_id: `${quiz.id}`,
-				user_id: `${user.id}`,
-				score: 100,
-			});
-		}
-	} catch (err) {
-		console.error(err);
-	}
-};
-
-/**
- * This script will insert users into the database
- *
- */
-const seedUsers = async () => {
-	try {
-		console.log('Seeding users');
-
-		for (const user of usersToInsert) {
-			const newUser = await db.insert(usersTable).values(user).returning({ id: usersTable.id });
-			const id = newUser[0].id;
-
-			await Promise.all([
-				db.insert(usersSettingsTable).values({
-					user_id: id,
-				}),
-				db.insert(usersStatisticsTable).values({
-					user_id: id,
-				}),
-				db.insert(usersTimestampsTable).values({
-					user_id: id,
-				}),
-				db.insert(usersBillingInformationTable).values({
-					user_id: id,
+				db.insert(schema.quizQuestionsTable).values({
+					quiz_id: id,
+					question: 'What is 2 + 2?',
+					answers: ['1', '2', '3', '4'],
+					correct_answer: '4',
 				}),
 			]);
-			await db
-				.update(usersSettingsTable)
-				.set({
-					roles: 'admin',
-          is_verified: true,
-				})
-				.where(eq(usersSettingsTable.user_id, id));
 		}
 	} catch (err) {
 		console.error(err);
 	}
 };
 
-const queryAll = async () => {
+const main = async () => {
 	try {
-		console.log('Querying all');
-
-		const users = await queryUsersWithAllTables();
-		console.log(users);
-		console.log('---------------------------------');
-		const quizzes = await queryQuizzesWithAllTables();
-		console.log(quizzes[0], quizzes[0].questions[0].answers);
-		console.log(quizzes[0].highscores);
+		await client.connect();
+		await clearDB(db);
+		await seedQuizzes();
+		await client.end();
 	} catch (err) {
 		console.error(err);
 	}
-};
-
-async function queryUsersWithAllTables() {
-	try {
-		const users = await db.query.usersTable.findMany({
-			with: {
-				billing_information: true,
-				statistics: true,
-				settings: true,
-				timestamps: true,
-			},
-		});
-		return users;
-	} catch (err) {
-		console.error(err);
-	}
-}
-
-async function queryQuizzesWithAllTables() {
-	try {
-		const quizzes = await db.query.quizzesTable.findMany({
-			with: {
-				highscores: true,
-				questions: true,
-			},
-		});
-		return quizzes;
-	} catch (err) {
-		console.error(err);
-	}
-}
-
-async function main() {
-	await client.connect();
-	await seedUsers();
-	await seedQuizzes();
-	await seedQuizWithHighscores();
-	await queryAll();
-	await client.end();
 }
 main();
