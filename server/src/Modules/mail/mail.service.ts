e7feefc85ead35ef222b779fd5e5f3aeaec8cec0
phil@home <philipp.millner@outlook.com>
2024-03-18 09:51:34 +0100

    [refac] added an eventListener to send verify mail Method

--- server/src/Modules/mail/mail.service.ts
+++ server/src/Modules/mail/mail.service.ts
@@ -1,40 +1,41 @@
-import { MailerService } from "@nestjs-modules/mailer";
-import { Injectable } from "@nestjs/common";
-import { SelectUser } from "src/Utils/Types/model.types";
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { SendVerifyMailEvent } from 'src/Events/send.verify.mail.event';
 
 @Injectable()
 export class MailService {
-    constructor(private readonly mailerService: MailerService) {}
+	constructor(private readonly mailerService: MailerService) {}
 
+	/**
+	 * @description - Listens to the mail.verify-email event to send a confirmation email to the user
+	 * @param {SelectUser} user - The user instance for which to send the confirmation email
+	 * @param {string} token - The token to verify the user's email
+	 * @returns {Promise<void>} - Returns void
+	 */
+    @OnEvent('mail.verify-email')
+	async sendConfirmationEmail(payLoad: SendVerifyMailEvent): Promise<void | Error> {
+		try {
+			const url = `http://localhost:3000/auth/verify-email/${payLoad.userID}/${payLoad.token}`;
 
-    /**
-     * @description - Sends a confirmation email to the user
-     * @param {SelectUser} user - The user instance for which to send the confirmation email
-     * @param {string} token - The token to verify the user's email
-     * @returns {Promise<void>} - Returns void
-     */
-    async sendConfirmationEmail(user: SelectUser, token: string): Promise<void | Error> {
-        try {
-            const url = `http://localhost:3000/auth/verify-email/${user.id}/${token}`;
-    
-            await this.mailerService.sendMail({
-                to: user.email,
-                subject: 'Willkommen bei Brain Blitz! Bitte bestätige deine E-Mail-Adresse',
-                template: './confirmation',
-                context: {
-                    name: user.first_name,
-                    url,
-                },
-                attachments: [
-                    {
-                        filename: 'email.svg',
-                        path: './dist/Public/SVGS/email.svg',
-                        cid: 'email',
-                    },
-                ]
-            })
-        } catch (error) {
-            return error
-        }
-    }
+			await this.mailerService.sendMail({
+				to: payLoad.email,
+				subject: 'Willkommen bei Brain Blitz! Bitte bestätige deine E-Mail-Adresse',
+				template: './confirmation',
+				context: {
+					name: payLoad.firstName,
+					url,
+				},
+				attachments: [
+					{
+						filename: 'email.svg',
+						path: './dist/Public/SVGS/email.svg',
+						cid: 'email',
+					},
+				],
+			});
+		} catch (error) {
+			return error;
+		}
+	}
 }
