a9fc441ea79b596bd3146a9998d06eaf1c319441
phil@home <philipp.millner@outlook.com>
2024-03-19 14:33:55 +0100

    [new] added 2 new mail events
    sendForgotPasswordMail sends an email if the user forgot its password
    sendPasswordChangedEmail sends an email if the user password was changed - to inform user if someone stole his account or not

--- server/src/Modules/mail/mail.service.ts
+++ server/src/Modules/mail/mail.service.ts
@@ -1,7 +1,7 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
-import { SendVerifyMailEvent } from 'src/Events/notification.events';
+import { PasswordChangedEvent, SendForgotPasswordMailEvent, SendVerifyMailEvent } from 'src/Events/notification.events';
 
 @Injectable()
 export class MailService {
@@ -9,8 +9,7 @@ export class MailService {
 
 	/**
 	 * @description - Listens to the mail.verify-email event to send a confirmation email to the user
-	 * @param {SelectUser} user - The user instance for which to send the confirmation email
-	 * @param {string} token - The token to verify the user's email
+	 * @param {SendVerifyMailEvent} payLoad - The user instance for which to send the confirmation email
 	 * @returns {Promise<void>} - Returns void
 	 */
     @OnEvent('mail.verify-email')
@@ -22,6 +21,71 @@ export class MailService {
 				to: payLoad.email,
 				subject: 'Willkommen bei Brain Blitz! Bitte bestätige deine E-Mail-Adresse',
 				template: './confirmation',
+				context: {
+					name: payLoad.firstName,
+					url,
+				},
+				attachments: [
+					{
+						filename: 'email.svg',
+						path: './dist/Public/SVGS/email.svg',
+						cid: 'email',
+					},
+				],
+			});
+		} catch (error) {
+			return error;
+		}
+	}
+
+	/**
+	 * @description - Listens to the mail.forgot-password event to send a password reset email to the user
+	 * @param {SendForgotPasswordMailEvent} payLoad - The user instance for which to send the password reset email
+	 * @returns {Promise<void>} - Returns void
+	 * @returns {Promise<Error>} - Returns an error if the email fails to send
+	 */
+	@OnEvent('mail.forgot-password')
+	async sendForgotPasswordEmail(payLoad: SendForgotPasswordMailEvent): Promise<void | Error> {
+		try {
+			const url = `${process.env.BASE_PASSWORD_RESET_URL}${payLoad.userID}/${payLoad.token}`;
+
+			await this.mailerService.sendMail({
+				to: payLoad.email,
+				subject: 'Passwort zurücksetzen',
+				template: './forgot-password',
+				context: {
+					name: payLoad.firstName,
+					url,
+				},
+				attachments: [
+					{
+						filename: 'email.svg',
+						path: './dist/Public/SVGS/email.svg',
+						cid: 'email',
+					},
+				],
+			});
+		} catch (error) {
+			return error;
+		}
+	}
+
+	/**
+	 * @description - Listens to the mail.password-changed event to send a password changed email to the user
+	 * @param {PasswordChangedEvent} payLoad - The user instance for which to send the password changed email
+	 * @returns {Promise<void>} - Returns void
+	 * @returns {Promise<Error>} - Returns an error if the email fails to send
+	 */
+	@OnEvent('mail.password-changed')
+	async sendPasswordChangedEmail(payLoad: PasswordChangedEvent): Promise<void | Error> {
+		try {
+
+			const url = `${process.env.BASE_CONTACT_URL}`;
+
+			await this.mailerService.sendMail({
+				to: payLoad.email,
+				subject: 'Passwort wurde geändert',
+				template: './password-changed',
 				context: {
 					name: payLoad.firstName,
 					url,
