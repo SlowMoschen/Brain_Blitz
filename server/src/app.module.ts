c011941fa2df47b34cfd3fa4912ae7c28ce2d404
phil@home <philipp.millner@outlook.com>
2024-03-21 20:38:37 +0100

    [new] installed and setup a basic throttler
    10 requests per minute

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,14 @@ import { QuizzesModule } from './Modules/quizzes/quizzes.module';
 import { SharedModule } from './Modules/shared/shared.module';
 import { UsersModule } from './Modules/users/users.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
 	imports: [
+		ThrottlerModule.forRoot([{
+			ttl: 60,
+			limit: 10,
+		}]),
 		ConfigModule.forRoot({ isGlobal: true }),
 		EventEmitterModule.forRoot(),
 		SharedModule,
