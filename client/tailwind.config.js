8fd3665911ddeaeb9a5f7161db452bd29ac87c41
phil@home <philipp.millner@outlook.com>
2024-03-26 12:40:55 +0100

    [misc] changed color setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,25 +6,14 @@ export default {
   ],
   theme: {
     colors: {
-      text : {
-        white: '#fcfcfc',
-        black: '#1a1a1a',
-      }, 
-      background: {
-        primary: '#1a1a1a',
-        secondary: '#2b2b2b',
-      },
-      primary: {
-        default: '#c10000',
-        light: '#241818',
-      },
-      secondary: {
-        default: '#c4c4c4',
-      },
-      accent: {
-        default: '#99ff66',
-        light: '#465c2f',
-      },
+     'bg-primary': '#1a1a1a',
+      'bg-secondary': '#2b2b2b',
+      'text-primary': '#fcfcfc',
+      'text-secondary': '#c4c4c4',
+      'primary': '#c10000',
+      'primary-light': '#241818',
+      'accent': '#99ff66',
+      'accent-light': '#465c2f',
     },
     extend: {},
   },
