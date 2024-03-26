f6bbb9fc534b69d2b7eb8b5047faf1193bc1f879
phil@home <philipp.millner@outlook.com>
2024-03-26 11:41:53 +0100

    [new] tailwind custom color setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      text : {
+        white: '#fcfcfc',
+        black: '#1a1a1a',
+      }, 
+      background: {
+        primary: '#1a1a1a',
+        secondary: '#2b2b2b',
+      },
+      primary: {
+        default: '#c10000',
+        light: '#241818',
+      },
+      secondary: {
+        default: '#c4c4c4',
+      },
+      accent: {
+        default: '#99ff66',
+        light: '#465c2f',
+      },
+    },
     extend: {},
   },
   plugins: [],
