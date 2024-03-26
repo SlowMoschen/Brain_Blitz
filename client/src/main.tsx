ea55e9aab347027ced5f9188df9b6fd23e66b0e6
phil@home <philipp.millner@outlook.com>
2024-03-26 15:15:12 +0100

    introduced root route with children routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
-import Root from './routes/home/pages/root.tsx'
-import ErrorPage from './routes/error/index.tsx'
+import ErrorPage from './routes/error/ErrorPage.tsx'
+import Root from './routes/home/pages/Root.tsx'
+import Home from './routes/home/pages/Home.tsx'
+import About from './routes/home/pages/About.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+    ],
   },
 ])
 
