870c302f4f52ec5a52972076c9a188ddb47e3dcd
phil@home <philipp.millner@outlook.com>
2024-03-26 15:12:59 +0100

    implemented root route with follwing children routes
    * Root.tsx - Layout - Renders children route content, navbar and footer
    * Home.tsx - Content - Homepage content
    *About.tsx - Content - About us content

--- client/src/routes/home/pages/root.tsx
+++ client/src/routes/home/pages/root.tsx
@@ -1,8 +1,28 @@
+import { Outlet } from "react-router-dom";
+import MobileRootNavbar from "../components/Navbar/MobileNavbar";
+import RootFooter from "../components/Footer";
+import { BREAKPOINTS } from "../../../shared/constants/breakpoints";
+import RootNavbar from "../components/Navbar/Navbar";
+import { useEffect, useState } from "react";
 
 export default function Root() {
-    return (
-        <>
-        <h1>Hello World</h1>
-        </>
-    )
+  const [deviceWidth, setDeviceWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setDeviceWidth(window.innerWidth));
+
+    return () => {
+      window.removeEventListener("resize", () => setDeviceWidth(window.innerWidth));
+    };
+  }, []);
+
+  return (
+    <>
+      {deviceWidth < BREAKPOINTS.md ? <MobileRootNavbar /> : <RootNavbar />}
+      <div id="detail" className="h-screen">
+        <Outlet />
+      </div>
+      <RootFooter />
+    </>
+  );
 }
