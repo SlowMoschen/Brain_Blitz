8f502fbf008d7e0d300e75bf3f4bd143c2c40a35
phil@home <philipp.millner@outlook.com>
2024-03-26 17:24:38 +0100

    changed folder structure - implemented WhySection Component
    Implemneted a WhySection Component to clean up Home Page component

--- client/src/routes/home/pages/Home.tsx
+++ client/src/routes/home/pages/Home.tsx
@@ -1,31 +1,11 @@
-import Header from "../../../shared/components/Header";
 import HeroSection from "../components/Hero/Hero";
-import WhyCard from "../components/WhyCard/WhyCard";
+import WhySection from "../components/WhySection/WhySection";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <HeroSection />
-      <section id="why-section" className="p-5 m-5">
-        <Header content="Warum Brain Blitz?" className="big" />
-        <section className="flex flex-col lg:flex-row justify-center items-center my-2">
-          <WhyCard
-            title="Wissen erweitern"
-            description="Spielerisch Wissen in verschiedenen Kategorien erweitern!"
-            icon="local_library"
-          />
-          <WhyCard
-            title="Jederzeit und Überall"
-            description="Lernen leicht gemacht, überall und jederzeit!"
-            icon="location_on"
-          />
-          <WhyCard
-            title="Bildung und Unterhaltung Kombiniert"
-            description="Faszinierende Fakten entdecken, Bildung und Unterhaltung vereint!"
-            icon="cast_for_education"
-          />
-        </section>
-      </section>
+      <WhySection />
     </>
   );
 }
