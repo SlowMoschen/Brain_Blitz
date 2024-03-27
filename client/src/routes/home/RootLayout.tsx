e1ac727f510fc372f4150a9e7023ded342d9668f
phil@home <philipp.millner@outlook.com>
2024-03-27 18:29:58 +0100

    [refac] Implemented useScreenSize Hook
    Refactored the RootLayout to use the hook

--- client/src/routes/home/RootLayout.tsx
+++ client/src/routes/home/RootLayout.tsx
@@ -1,24 +1,16 @@
-import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { BREAKPOINTS } from "../../shared/constants/breakpoints";
+import useScreenSize from "../../shared/hooks/useScreenSize";
 import RootFooter from "./components/Footer/Footer";
 import MobileRootNavbar from "./components/Navbar/MobileNavbar";
 import RootNavbar from "./components/Navbar/Navbar";
 
 export default function RootLayout() {
-  const [deviceWidth, setDeviceWidth] = useState<number>(window.innerWidth);
-
-  useEffect(() => {
-    window.addEventListener("resize", () => setDeviceWidth(window.innerWidth));
-
-    return () => {
-      window.removeEventListener("resize", () => setDeviceWidth(window.innerWidth));
-    };
-  }, []);
+  const screenSize = useScreenSize();
 
   return (
     <>
-      {deviceWidth <= BREAKPOINTS.md ? <MobileRootNavbar /> : <RootNavbar />}
+      {screenSize.width <= BREAKPOINTS.md ? <MobileRootNavbar /> : <RootNavbar />}
       <main id="content" className="h-fit flex flex-col items-center">
         <Outlet />
       </main>
