0fee3c26ef2d78ee250f9a978280c6ab47421abd
phil@home <philipp.millner@outlook.com>
2024-03-27 19:35:35 +0100

    [new] setup data fetch for about us page

--- client/src/routes/home/pages/about/About.tsx
+++ client/src/routes/home/pages/about/About.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import { HttpService } from "../../../../shared/services/httpService";
+import { APPLICATION } from "../../../../shared/constants/application";
+import useError from "../../../../shared/hooks/useError";
 
-export default function AboutPage() {
-  return (
-    <div>
-      <h1>About</h1>
-      <p>This is the about page.</p>
-    </div>
-  );
+interface QuizData {
+  uniqueCategories: number;
+  totalQuestions: number;
+  totalQuizzes: number;
+  categoryStats: {
+    category: string;
+    totalQuestions: number;
+    totalQuizzes: number;
+  }
+}
+
+export default function AboutPage(): JSX.Element {
+  const [quizData, setQuizData] = useState<QuizData[]>([]);
+  const [error, handleError] = useError(false);
+
+  const fetchQuizData = async () => {
+    const httpService = new HttpService();
+    try {
+      const data = await httpService.get(APPLICATION.QUIZ_DATA_ENDPOINT);
+      setQuizData(data);
+    } catch (error) {
+      handleError("Failed to fetch quiz data");
+    }
+  };
+
+  useEffect(() => {
+    fetchQuizData();
+    // disable the eslint warning for the next line - it's intentional - we only want to run this effect once
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return <h1>About</h1>;
 }
