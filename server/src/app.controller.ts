1bce2cda55170e9b0c5477d02db958f2823fa832
phil@home <philipp.millner@outlook.com>
2024-03-28 20:22:45 +0100

    App controller now will skip throttler

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from "@nestjs/common";
 import { QuizService } from "./Modules/quizzes/quizzes.service";
+import { SkipThrottle } from "@nestjs/throttler";
 
+@SkipThrottle()
 @Controller()
 export class AppController {
     constructor(private readonly quizService: QuizService) {}
