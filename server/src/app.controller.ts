75fff5465f43e4a77f2e6c0dd1844073a293326d
phil@home <philipp.millner@outlook.com>
2024-03-29 23:11:55 +0100

    [new] implemented a route for contact page

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { QuizService } from "./Modules/quizzes/quizzes.service";
 import { SkipThrottle } from "@nestjs/throttler";
 
@@ -15,5 +15,11 @@ export class AppController {
     @Get('quiz-data')
     async getStats() {
         return await this.quizService.getStats();
+    }
+
+    @Post('contact')
+    async sendContactForm(@Body() body: any) {
+        console.log(body);
+        return 'Contact form submitted!';
     }
 }
