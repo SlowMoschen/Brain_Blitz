761a8171ca738cd218626776327e7dc6e3ba1f7a
phil@home <philipp.millner@outlook.com>
2024-03-29 23:26:13 +0100

    [new] contact enpoint does emit an event now to trigger email sendings

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,25 +1,30 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { QuizService } from "./Modules/quizzes/quizzes.service";
-import { SkipThrottle } from "@nestjs/throttler";
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { QuizService } from './Modules/quizzes/quizzes.service';
+import { SkipThrottle } from '@nestjs/throttler';
+import { ContactDTO } from './contact.dto';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @SkipThrottle()
 @Controller()
 export class AppController {
-    constructor(private readonly quizService: QuizService) {}
+	constructor(
+		private readonly quizService: QuizService,
+		private readonly eventEmitter: EventEmitter2,
+	) {}
 
-    @Get()
-    healthCheck(): string {
-        return 'Server is up and running!'
-    }
+	@Get()
+	healthCheck(): string {
+		return 'Server is up and running!';
+	}
 
-    @Get('quiz-data')
-    async getStats() {
-        return await this.quizService.getStats();
-    }
+	@Get('quiz-data')
+	async getStats() {
+		return await this.quizService.getStats();
+	}
 
-    @Post('contact')
-    async sendContactForm(@Body() body: any) {
-        console.log(body);
-        return 'Contact form submitted!';
-    }
+	@Post('contact')
+	async sendContactForm(@Body() contactDTO: ContactDTO) {
+        this.eventEmitter.emit('mail.contact-form', contactDTO);
+		return 'Contact form submitted!';
+	}
 }
