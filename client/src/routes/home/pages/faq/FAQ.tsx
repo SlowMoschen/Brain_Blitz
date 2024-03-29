ca1159294f2aa4849ecf249ddd216eb5a5fde943
phil@home <philipp.millner@outlook.com>
2024-03-29 21:51:29 +0100

    [new] implemented faq page with faq accordion components

---
+++ client/src/routes/home/pages/faq/FAQ.tsx
@@ -0,0 +1,47 @@
+import Article from "../../../../shared/components/Article";
+import FaqAccordion from "./FAQAccordion";
+
+export default function FAQ(): JSX.Element {
+  return (
+    <>
+      <Article
+        title={{ content: "Häufig gestellte Fragen", border_b: true }}
+        para1="Unsere FAQ-Seite bietet eine kompakte Zusammenfassung von Informationen und Antworten auf gängige Fragen zur Nutzung unserer Quiz-App."
+      />
+      <ul className="mx-5">
+        <FaqAccordion
+          title="Ist die App kostenlos?"
+          content="Ja, unsere Quiz-App ist kostenlos verfügbar zum Spielen, es gibt keine versteckten Kosten. Doch bald kannst du uns mit unseren Support-Mitgliedschaften unterstützen."
+        />
+        <FaqAccordion
+          title="Unterstützt die App verschiedene Sprachen?"
+          content="Derzeit bieten wir unsere Quiz-App nur in einer Sprache an. Wir arbeiten jedoch daran, Unterstützung für weitere Sprachen hinzuzufügen, um unsere Benutzer weltweit besser zu bedienen."
+        />
+        <FaqAccordion
+          title="Wie funktionieren die Punktevergabe und Zeitlimits in den Quizzen?"
+          content="In unseren Quizzen erhalten Spieler Punkte für richtige Antworten und verlieren Punkte für falsche Antworten. Zusätzlich gibt es Zeitlimits für jedes Quiz, wobei verbleibende Zeit auch Bonuspunkte einbringt, um die Spannung in der Rangliste aufrechtzuerhalten."
+        />
+        <FaqAccordion
+          title="Werden meine Highscores gespeichert?"
+          content="Ja, sobald ein Quiz abgeschlossen ist, wird dein Highscore gespeichert. Du kannst deine Leistungen in den Ranglisten vergleichen und verfolgen."
+        />
+        <FaqAccordion
+          title="Kann ich die Rangliste anderer Spieler sehen?"
+          content="Ja, sobald du ein Quiz abgeschlossen hast, kannst du in die Rangliste einsehen und verfolgen wie du im Vergleich zu anderen Spielern abschneidest."
+        />
+        <FaqAccordion
+          title="Gibt es verschiedene Kategorien von Quizzen?"
+          content="Ja, unsere Quiz-App bietet eine breite Palette von Kategorien wie Geschichte, Popkultur, Biologie, Universum, Geo-Politik und mehr, um sicherzustellen, dass für jeden etwas dabei ist."
+        />
+        <FaqAccordion
+          title="Kann ich eigene Quizze erstellen?"
+          content="Derzeit unterstützen wir keine benutzerdefinierten Quizze, aber wir arbeiten daran, diese Funktion in Zukunft hinzuzufügen, um unseren Benutzern mehr Kontrolle über den Inhalt zu geben."
+        />
+        <FaqAccordion
+          title="Wie kann ich Feedback oder Vorschläge zur App abgeben?"
+            content="Wir schätzen das Feedback unserer Benutzer sehr! Du kannst uns dein Feedback oder deine Vorschläge gerne über unser Kontaktformular auf der App-Website mitteilen. Wir freuen uns darauf, von dir zu hören und unsere App kontinuierlich zu verbessern."
+        />
+      </ul>
+    </>
+  );
+}
