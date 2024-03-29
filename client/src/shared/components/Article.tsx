d5e52a5a74b1f17ea91cdf7524a54805d6510173
Millner Philipp <philipp.millner@outlook.com>
2024-03-29 11:16:27 +0100

    [new] implemented a reusable Article Component
    The Article takes a title with option, and 3 paragraphs

---
+++ client/src/shared/components/Article.tsx
@@ -0,0 +1,42 @@
+interface ArticleProps {
+  containerClass?: string;
+  title: {
+    content: string;
+    textColor?:
+      | "text-text-primary"
+      | "text-text-secondary"
+      | "text-primary"
+      | "animated-bg";
+    "border-b": boolean;
+  };
+  para1: string;
+  para2?: string;
+  para3?: string;
+  textColor?: "text-text-primary" | "text-text-secondary";
+}
+
+function Article({
+    containerClass = "",
+  title,
+  para1,
+  para2,
+  para3,
+  textColor = "text-text-primary",
+}: ArticleProps) {
+  return (
+    <article className={`flex flex-col items-center justify-center w-full h-full p-4 max-w-5xl ${containerClass}`}>
+      <h1
+        className={`text-4xl font-bold text-center my-3 ${title.textColor} ${
+          title["border-b"] ? "border-b-8 border-accent-light" : ""
+        }`}
+      >
+        {title.content}
+      </h1>
+      <p className={`text-lg my-2 ${textColor}`}>{para1}</p>
+      {para2 && <p className={`text-lg my-2 ${textColor}`}>{para2}</p>}
+      {para3 && <p className={`text-lg my-2 ${textColor}`}>{para3}</p>}
+    </article>
+  );
+}
+
+export default Article;
