e9287dc6c09ebfb3970ca397927b6b3809ba3dbb
Millner Philipp <philipp.millner@outlook.com>
2024-03-29 13:03:22 +0100

    [new] Layout now uses the ScrollToTop component

--- client/src/routes/home/RootLayout.tsx
+++ client/src/routes/home/RootLayout.tsx
@@ -4,12 +4,14 @@ import useScreenSize from "../../shared/hooks/useScreenSize";
 import RootFooter from "./components/Footer/Footer";
 import MobileRootNavbar from "./components/Navbar/MobileNavbar";
 import RootNavbar from "./components/Navbar/Navbar";
+import ScrollToTop from "../../shared/components/ScrollToTop";
 
 export default function RootLayout() {
   const screenSize = useScreenSize();
 
   return (
     <>
+      <ScrollToTop />
       {screenSize.width <= BREAKPOINTS.md ? <MobileRootNavbar /> : <RootNavbar />}
       <main id="content" className="h-fit flex flex-col items-center">
         <Outlet />
