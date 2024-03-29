830a3496c61e297f40e02e15c5bfc8b01d2abb9a
Millner Philipp <philipp.millner@outlook.com>
2024-03-29 11:58:00 +0100

    [new] implemented imprint page

---
+++ client/src/routes/home/pages/imprint/Imprint.tsx
@@ -0,0 +1,42 @@
+export default function Imprint() {
+  return (
+    <section className="bg-bg-secondary w-5/6 max-w-4xl p-10 m-10 rounded-lg flex flex-col justify-center items-center">
+      <h1 className="text-2xl border-b-8 border-accent-light text-center">Verantwortlich für den Inhalt dieser Quiz-App:</h1>
+
+      <div className="bg-secondary w-full max-w-md p-5 m-3 rounded-lg text-bg-secondary items-center">
+        <p className="text-xl underline">Kontakt:</p>
+        <p>Philipp Millner</p>
+        <p>Hütteldorferstraße 185</p>
+        <p>1140 Wien</p>
+        <p>+43 676 9487 225</p>
+        <p>service@brain-blitz.com</p>
+      </div>
+
+      <p className="leading-7 tracking-wide my-3">
+        Die Europäische Kommission stellt eine Plattform zur
+        Online-Streitbeilegung (OS) bereit, die Sie unter {" "} 
+        <a className="text-primary break-all" href="http://ec.europa.eu/consumers/odr" target="_blank">
+          http://ec.europa.eu/consumers/odr
+        </a>{" "}
+        finden. Wir sind nicht bereit oder verpflichtet, an
+        Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle
+        teilzunehmen.
+      </p>
+
+      <p className="leading-7 tracking-wide my-10 text-lg w-full">
+        Diese Angaben gelten auch für unsere {" "}
+        <a className="text-primary break-all" href="https://linktr.ee/BrainBlitz" target="_blank">Social-Media-Profile</a>
+      </p>
+
+      <p className="leading-7 tracking-wide my-3">
+        Wir weisen darauf hin, dass jegliche unerlaubte Verwendung von Inhalten
+        unserer App oder Teilen davon, einschließlich der Vervielfältigung,
+        Verbreitung oder Verwendung in anderen elektronischen oder gedruckten
+        Publikationen, ohne unsere ausdrückliche Zustimmung rechtliche Schritte
+        nach sich ziehen kann.
+      </p>
+
+      <p className="text-sm mt-3 opacity-50">Dieses Impressum wurde zuletzt am 29.03.2024 aktualisiert.</p>
+    </section>
+  );
+}
