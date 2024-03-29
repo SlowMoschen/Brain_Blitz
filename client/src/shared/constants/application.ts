9bc6557800a431788dfff34b0098c683159adbce
Millner Philipp <philipp.millner@outlook.com>
2024-03-29 11:15:39 +0100

    [new] added color constant object

--- client/src/shared/constants/application.ts
+++ client/src/shared/constants/application.ts
@@ -2,3 +2,14 @@ export const APPLICATION ={
     API_URL: "http://localhost:3000",
     QUIZ_DATA_ENDPOINT: "/quiz-data",
 }
+
+export const COLORS = {
+    BG_PRIMARY: "#1a1a1a",
+    BG_SECONDARY: "#2b2b2b",
+    TEXT_PRIMARY: "#fcfcfc",
+    TEXT_SECONDARY: "#1a1a1a",
+    PRIMARY: "#c10000",
+    PRIMARY_LIGHT: "#241818",
+    ACCENT: "#99ff66",
+    ACCENT_LIGHT: "#465c2f",
+}
