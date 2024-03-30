31306899dd05a16ca9cddb1f366f89e5f6eb4ba3
phil@home <philipp.millner@outlook.com>
2024-03-31 00:20:18 +0100

    [new] implemented a visibility option for password inputs

--- client/src/shared/components/Input.tsx
+++ client/src/shared/components/Input.tsx
@@ -1,23 +1,49 @@
+import useToggle from "../hooks/useToggle";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    type: 'text' | 'email' | 'password';
-    placeholder: string;
-    className: string;
+  type: "text" | "email" | "password";
+  placeholder: string;
+  className: string;
 }
 
-export default function Input({ type, placeholder, className, name, onChange, ...rest }: InputProps) {
+export default function Input({
+  type,
+  placeholder,
+  className,
+  name,
+  onChange,
+  ...rest
+}: InputProps) {
+  const [showPassword, toggleShowPassword] = useToggle(false);
+  const capitalize = (s: string | undefined) => {
+    if (!s) return "";
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  };
 
-    const capitalize = (s: string | undefined) => {
-        if (!s) return '';
-        return s.charAt(0).toUpperCase() + s.slice(1);
-    }
-
-    return (
-        <>
-        <div className="flex flex-col w-full p-5">
-            <label htmlFor={name} className="text-xl my-1">{capitalize(name)}</label>
-            <input type={type} name={name} placeholder={placeholder} className={className} onChange={onChange} {...rest} />
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex flex-col w-full p-2 relative">
+        <label htmlFor={name} className="text-xl my-1">
+          {capitalize(name)}
+        </label>
+        <input
+          type={type === "password" ? (showPassword ? "text" : "password") : type}
+          name={name}
+          placeholder={placeholder}
+          className={className}
+          onChange={onChange}
+          {...rest}
+        />
+        {rest.required && <p className="text-xs opacity-50 my-1">*Pflichtfeld</p>}
+        {type === "password" && (
+          <span
+            className="material-symbols-outlined text-bg-primary absolute right-4 top-12 cursor-pointer"
+            onClick={() => toggleShowPassword()}
+          >
+            {showPassword ? "visibility_off" : "visibility"}
+          </span>
+        )}
+      </div>
+    </>
+  );
 }
