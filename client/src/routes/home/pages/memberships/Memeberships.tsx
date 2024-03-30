e96bca360f3267df26a3e94a5029dedaefc8e779
phil@home <philipp.millner@outlook.com>
2024-03-30 21:14:48 +0100

    changed background color system for membership card

--- client/src/routes/home/pages/memberships/Memeberships.tsx
+++ client/src/routes/home/pages/memberships/Memeberships.tsx
@@ -6,42 +6,42 @@ import { BREAKPOINTS } from "../../../../shared/constants/breakpoints";
 import { useNavigate } from "react-router-dom";
 
 export default function Memberships() {
+  const screenSize = useScreenSize();
+  const redirect = useNavigate();
 
-    const screenSize = useScreenSize();
-    const redirect = useNavigate();
+  const defaultFeatures = [
+    "unbegrenztes spielen von Quizzes",
+    "Highscores werden gespeichert",
+    "Ranglisten",
+  ];
 
-    const defaultFeatures = [
-        "unbegrenztes spielen von Quizzes",
-        "Highscores werden gespeichert",
-        "Ranglisten",
-    ];
-
-    const memberships = [
-        {
-            title: "Basic",
-            price: "Gratis",
-            features: defaultFeatures,
-            btnOnClick: () => redirect('/auth/register'),
-            btnText: "Jetzt loslegen",
-            isAvailable: true
-        },
-        {
-            title: "Support",
-            price: "TBA",
-            features: [...defaultFeatures, "Priorisierter Support"],
-            btnOnClick: () => {},
-            btnText: "Jetzt beitreten",
-            isAvailable: false
-        },
-        {
-            title: "Team",
-            price: "TBA",
-            features: [...defaultFeatures, "Team-Features"],
-            btnOnClick: () => {},
-            btnText: "Jetzt beitreten",
-            isAvailable: false
-        }
-    ];
+  const memberships = [
+    {
+      title: "Basic",
+      price: "Gratis",
+      features: defaultFeatures,
+      btnOnClick: () => redirect("/auth/register"),
+      btnText: "Jetzt loslegen",
+      isAvailable: true,
+    },
+    {
+      title: "Support",
+      price: "TBA",
+      features: [...defaultFeatures, "Priorisierter Support"],
+      btnOnClick: () => {},
+      btnText: "Jetzt beitreten",
+      isAvailable: false,
+      secondaryBGColor: true,
+    },
+    {
+      title: "Team",
+      price: "TBA",
+      features: [...defaultFeatures, "Team-Features"],
+      btnOnClick: () => {},
+      btnText: "Jetzt beitreten",
+      isAvailable: false,
+    },
+  ];
 
   return (
     <>
@@ -50,12 +50,23 @@ export default function Memberships() {
         para1="
         Aktuell bieten wir ausschließlich unsere kostenlose Mitgliedschaft an, die auch dauerhaft verfügbar sein wird. Zusätzlich arbeiten wir derzeit an der Entwicklung einer Support-Mitgliedschaft, um unseren Nutzern noch mehr Vorteile und Funktionen bieten zu können. Darüber hinaus befindet sich auch eine Team-Mitgliedschaft in der Entwicklung, um Gruppen und Teams eine verbesserte Nutzungserfahrung zu ermöglichen. Bleiben Sie gespannt auf weitere Updates und Features!"
       />
-      <div className={clsx(
-        'w-full my-10',
-        screenSize.width >= BREAKPOINTS.lg ? 'grid grid-cols-3' : 'flex flex-col'
-      )}>
+      <div
+        className={clsx(
+          "w-full my-10",
+          screenSize.width >= BREAKPOINTS.lg ? "grid grid-cols-3" : "flex flex-col"
+        )}
+      >
         {memberships.map((membership, index) => (
-            <MembershipCard key={index} containerBGColor="bg-bg-secondary" title={membership.title} price={membership.price} features={membership.features} btnOnClick={membership.btnOnClick} btnText={membership.btnText} isAvailable={membership.isAvailable} />
+          <MembershipCard
+            key={index}
+            secondaryBGColor={membership.secondaryBGColor}
+            title={membership.title}
+            price={membership.price}
+            features={membership.features}
+            btnOnClick={membership.btnOnClick}
+            btnText={membership.btnText}
+            isAvailable={membership.isAvailable}
+          />
         ))}
       </div>
     </>
