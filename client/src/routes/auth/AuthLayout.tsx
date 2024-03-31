44db718f6f643a67731d13f250775610b278a237
phil@home <philipp.millner@outlook.com>
2024-03-31 04:25:33 +0200

    removed AuthLayout

--- client/src/routes/auth/AuthLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function AuthLayout() {
-  return (
-    <main className="flex flex-col justify-center items-center h-screen">
-      <Outlet />
-    </main>
-  );
-}
