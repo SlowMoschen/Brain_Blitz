4e16f3f49415b0dad5e48a1e8e011a4236c095ca
phil@home <philipp.millner@outlook.com>
2024-03-31 01:18:57 +0100

    [new] implemented Login Page

--- client/src/routes/auth/pages/login/Login.tsx
+++ client/src/routes/auth/pages/login/Login.tsx
@@ -1,34 +1,97 @@
+import { useNavigate } from "react-router-dom";
 import Form from "../../../../shared/components/Form";
 import Input from "../../../../shared/components/Input";
+import Logo from "../../../../shared/components/Logo";
+import MessageBox from "../../../../shared/components/MessageBox";
+import useError from "../../../../shared/hooks/useError";
 import { HttpService } from "../../../../shared/services/httpService";
+import { ValidationService } from "../../../../shared/services/validationService";
 
 export default function Login() {
-    const httpService = new HttpService();
+  const httpService = new HttpService();
+  const [error, handleError] = useError(true);
+  const redirect = useNavigate();
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // const form = event.currentTarget;
-        // const formData = new FormData(form);
-        // const email = formData.get("email") as string;
-        // const password = formData.get("password") as string;
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const email = formData.get("email")?.toString().toLowerCase() as string;
+    const password = formData.get("password")?.toString() as string;
 
-        try {
-            const response = await httpService.post("/auth/login", { email: 'philipp.millner@outlook.com', password: 'Test12345!' });
-            if (response.data) {
-                console.log(response.data);
-                return;
-            }
+    try {
+      await ValidationService.validate(email, ["isRequired", "isEmail"]);
+      await ValidationService.validate(password, ["isRequired", "minLen", "maxLen", "isPassword"]);
 
-            throw new Error(response.message);
-        } catch (error) {
-            console.error(error);
+      const response = await httpService.post("/auth/login", {
+        email,
+        password,
+      });
+      if (response.data) {
+        console.log(response.data);
+        redirect("/dashboard");
+        return;
+      }
+
+      throw new Error(response.message);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof Error) {
+        if (error.message === "Not Found") {
+          handleError("Benutzer nicht gefunden oder Passwort falsch. Bitte versuchen Sie es erneut.");
+          return;
         }
-    }
 
-    return (
-        <Form  onSubmit={ (e) => handleSubmit(e) }>
-            <Input type="email" placeholder="email" className="text-bg-primary"  />
-            <Input type="password" placeholder="password" className="text-bg-primary" />
-        </Form>
-    )
+        if (error.message === "Network Error") {
+          handleError("Netzwerkfehler. Bitte versuchen Sie es später erneut.");
+          return;
+        }
+        handleError(error.message);
+      }
+    }
+  };
+
+  return (
+    <>
+      <Logo maxHeight="120px" maxWidth="200px" className="my-2" />
+      <Form
+        onSubmit={(e) => handleSubmit(e)}
+        className="bg-bg-secondary py-5 px-2 m-5 rounded-lg w-11/12 max-w-5xl flex flex-col items-center"
+        btnText="Login"
+      >
+        <h1 className="text-3xl font-bold border-b-4 border-accent-light w-full px-2">Login</h1>
+        <p className="text-sm mt-2 px-2 opacity-50 w-full">
+          Willkommen zurück 👋! Bitte melde dich an.
+        </p>
+        <Input
+          type="email"
+          placeholder="Muster@email.com"
+          className="text-bg-primary rounded-md py-1 px-2 outline-none"
+          name="email"
+          required
+        />
+        <Input
+          type="password"
+          placeholder="min. 8 Zeichen"
+          className="text-bg-primary rounded-md py-1 px-2 outline-none"
+          name="password"
+          required
+        />
+        <div className="my-3 w-full">
+          <p className="text-sm px-2 mt-1 w-full">
+            <a href="/auth/forgot-password" className="text-primary underline">
+              Passwort vergessen?
+            </a>
+          </p>
+          <p className="text-sm px-2 mt-1 w-full">
+            Noch keinen Account?{" "}
+            <a href="/auth/register" className="text-primary underline">
+              Registrieren
+            </a>
+          </p>
+        </div>
+        <MessageBox message={error} className="min-h-11 text-primary" />
+      </Form>
+    </>
+  );
 }
