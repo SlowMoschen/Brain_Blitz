6098385230909945733934a728fd6b5d86651cfa
phil@home <philipp.millner@outlook.com>
2024-04-01 23:15:58 +0200

    removed container

--- material-ui-test/src/routes/home/RootLayout.tsx
+++ material-ui-test/src/routes/home/RootLayout.tsx
@@ -1,10 +1,9 @@
 import { Outlet } from "react-router-dom";
-import { Container } from "@mui/material";
-import useScreenSize from "../../shared/hooks/useScreenSize";
 import { BREAKPOINTS } from "../../configs/Breakpoints";
+import useScreenSize from "../../shared/hooks/useScreenSize";
 import Footer from "./components/Footer";
-import Navbar from "./components/navbar/_Navbar.main";
 import MobileNavbar from "./components/navbar/_MobileNavbar.main";
+import Navbar from "./components/navbar/_Navbar.main";
 
 export default function RootLayout() {
   const { width } = useScreenSize();
@@ -12,10 +11,8 @@ export default function RootLayout() {
   return (
     <>
       {width > BREAKPOINTS.md ? <Navbar /> : <MobileNavbar />}
-      <Container sx={{ height: '100vh', p: 2 }}>
         <Outlet />
       <Footer />
-      </Container>
     </>
   );
 }
