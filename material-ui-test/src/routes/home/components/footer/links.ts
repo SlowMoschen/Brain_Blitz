46383625db7f249d8d51335b901435494c7fa280
phil@home <philipp.millner@outlook.com>
2024-04-02 01:03:06 +0200

    implemented footer

---
+++ material-ui-test/src/routes/home/components/footer/links.ts
@@ -0,0 +1,58 @@
+import { URLS } from "../../../../configs/Links";
+
+export const navigationLinks = [
+  {
+    text: "Startseite",
+    to: URLS.HOME,
+  },
+  {
+    text: "Über uns",
+    to: URLS.ABOUT,
+  },
+  {
+    text: "F A Q",
+    to: URLS.FAQ,
+  },
+  {
+    text: "Mitgliedschaften",
+    to: URLS.MEMBERSHIPS,
+  },
+];
+
+export const accountLinks = [
+  {
+    text: "Login",
+    to: URLS.LOGIN,
+  },
+  {
+    text: "Registrieren",
+    to: URLS.REGISTER,
+  },
+  {
+    text: "Passwort vergessen",
+    to: URLS.FORGOT_PASSWORD,
+  },
+  {
+    text: "Verifizierung erneut senden",
+    to: URLS.RESEND_VERIFICATION_EMAIL,
+  },
+];
+
+export const legalLinks = [
+  {
+    text: "Impressum",
+    to: URLS.IMPRINT,
+  },
+  {
+    text: "Datenschutz",
+    to: URLS.PRIVACY,
+  },
+  {
+    text: "AGB",
+    to: URLS.TERMS_AND_CONDITIONS,
+  },
+  {
+    text: "Kontakt",
+    to: URLS.CONTACT,
+  },
+];
