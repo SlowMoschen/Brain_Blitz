5339909350a2a8f84c970889cb892dfede65fc07
phil@home <philipp.millner@outlook.com>
2024-04-02 01:30:58 +0200

    implemented error page

---
+++ material-ui-test/src/routes/error/ErrorPage.tsx
@@ -0,0 +1,25 @@
+import { Container, Link, Typography } from "@mui/material";
+import logoNoText from "../../assets/logoNoText.png";
+
+export default function ErrorPage() {
+
+    const containerStyle = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+    };
+
+    return (
+        <>
+        <Container sx={{ ...containerStyle }}>
+            <img src={logoNoText} alt="Brain Blitz Logo without Text" />
+            <Typography variant="h1" className="animated-bg" fontWeight={600} fontSize={150}>404</Typography>
+            <Typography variant="h2" fontSize={20} sx={{ opacity: '50%', mb: 2 }}>Seite nicht gefunden</Typography>
+            <Typography variant="body1">Die angeforderte Seite ist nicht verfügbar oder es ist ein Fehler aufgetretten.</Typography>
+            <Typography variant="body1"><Link>Gehe zurück</Link> oder <Link>kontaktiere unseren Support</Link></Typography>
+        </Container>
+            </>
+    )
+}
