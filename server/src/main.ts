9f2ff1d82740869948f8b227c0cb1a2900aba38c
phil@home <philipp.millner@outlook.com>
2024-04-02 10:45:03 +0200

    added localhost 5174 to cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,7 +16,7 @@ const pgSession = connectPGSession(session);
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
 		cors: {
-			origin: 'http://localhost:5173',
+			origin: ['http://localhost:5173', 'http://localhost:5174'],
 			credentials: true,
 		},
 	});
