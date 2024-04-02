00149b5ab2041fbaeee109f8e0e2b13a6769a7e0
phil@home <philipp.millner@outlook.com>
2024-04-02 10:42:58 +0200

    time constants copied

---
+++ material-ui-test/src/configs/Application.ts
@@ -0,0 +1,9 @@
+export const APPLICATION = {
+    BACKEND_URL: "http://localhost:3000",
+    QUIZ_DATA_ENDPOINT: "/quiz-data",
+}
+
+export const TIMES = {
+    COUNT_UP_DELAY: 0,
+    COUNT_UP_DURATION: 2,
+}
