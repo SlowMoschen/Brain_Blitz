bec1c0109d50547436ac3514a415ded71afab25f
phil@home <philipp.millner@outlook.com>
2024-04-02 12:09:12 +0200

    setup for faq children route

--- material-ui-test/src/App.tsx
+++ material-ui-test/src/App.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "@emotion/react";
 import customTheme from "./configs/CustomTheme";
 import { CssBaseline } from "@mui/material";
 import ErrorPage from "./routes/error/ErrorPage";
+import About from "./routes/home/pages/about/About";
+import FAQ from "./routes/home/pages/faq/FAQ";
 
 export default function App() {
   const router = createBrowserRouter([
@@ -16,7 +18,11 @@ export default function App() {
     {
       path: "/",
       element: <RootLayout />,
-      children: [{ path: "/", element: <Home /> }],
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/about", element: <About /> },
+        { path: "/faq", element: <FAQ />}
+      ],
     },
   ]);
 
