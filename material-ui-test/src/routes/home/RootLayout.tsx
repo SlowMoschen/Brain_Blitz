e9a0d45dbf2c343328c64366b5f8ecc166ebf482
phil@home <philipp.millner@outlook.com>
2024-04-02 10:44:28 +0200

    DataSection and EndSection implemented

--- material-ui-test/src/routes/home/RootLayout.tsx
+++ material-ui-test/src/routes/home/RootLayout.tsx
@@ -4,14 +4,24 @@ import useScreenSize from "../../shared/hooks/useScreenSize";
 import Footer from "./components/footer/Footer";
 import MobileNavbar from "./components/navbar/_MobileNavbar.main";
 import Navbar from "./components/navbar/_Navbar.main";
+import { useQuery } from "@tanstack/react-query";
+import { APPLICATION } from "../../configs/Application";
 
 export default function RootLayout() {
   const { width } = useScreenSize();
 
+  const fetchQuizData = async () => {
+    const response = await fetch(APPLICATION.BACKEND_URL + APPLICATION.QUIZ_DATA_ENDPOINT);
+    const data = await response.json();
+    return data;
+}
+
+const { data: response } = useQuery({ queryKey: ['quizData'], queryFn: fetchQuizData });
+
   return (
     <>
       {width > BREAKPOINTS.md ? <Navbar /> : <MobileNavbar />}
-        <Outlet />
+        <Outlet context={response?.data} />
       <Footer />
     </>
   );
