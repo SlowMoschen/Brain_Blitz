5279bb7b9980e97d0c77da6114c0ba9839d437c9
phil@home <philipp.millner@outlook.com>
2024-04-02 12:10:55 +0200

    added new Components * Added a ScrollToTop Components to always scroll to top when changing routes * Added a Container box for content - to position images relative to it

--- material-ui-test/src/routes/home/RootLayout.tsx
+++ material-ui-test/src/routes/home/RootLayout.tsx
@@ -6,6 +6,8 @@ import MobileNavbar from "./components/navbar/_MobileNavbar.main";
 import Navbar from "./components/navbar/_Navbar.main";
 import { useQuery } from "@tanstack/react-query";
 import { APPLICATION } from "../../configs/Application";
+import { Box } from "@mui/material";
+import ScrollToTop from "../../shared/components/ScrollToTop";
 
 export default function RootLayout() {
   const { width } = useScreenSize();
@@ -14,14 +16,27 @@ export default function RootLayout() {
     const response = await fetch(APPLICATION.BACKEND_URL + APPLICATION.QUIZ_DATA_ENDPOINT);
     const data = await response.json();
     return data;
-}
+  };
 
-const { data: response } = useQuery({ queryKey: ['quizData'], queryFn: fetchQuizData });
+  const { data: response } = useQuery({ queryKey: ["quizData"], queryFn: fetchQuizData });
 
   return (
     <>
+      <ScrollToTop />
       {width > BREAKPOINTS.md ? <Navbar /> : <MobileNavbar />}
+      <Box
+        sx={{
+          height: "fit-content",
+          width: "100%",
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Outlet context={response?.data} />
+      </Box>
       <Footer />
     </>
   );
