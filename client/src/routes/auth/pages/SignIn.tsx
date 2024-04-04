615f7581b060463fe91247de697b0aa07bd3d04a
Millner Philipp <philipp.millner@outlook.com>
2024-04-04 17:21:38 +0200

    misc changes - tried rect-hook-form and zod here before implementing it in the contact form

--- client/src/routes/auth/pages/SignIn.tsx
+++ client/src/routes/auth/pages/SignIn.tsx
@@ -1,50 +1,13 @@
 import {
-  Box,
-  FilledInput,
-  FormControl,
-  FormLabel,
-  IconButton,
-  TextField,
-  Typography,
+  Paper,
+  Typography
 } from "@mui/material";
-import useToggle from "../../../shared/hooks/useToggle.hook";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import CallToAction from "../../../shared/components/buttons/CallToAction";
 
 export default function SignIn() {
-  const [showPassword, setShowPassword] = useToggle(false);
 
   return (
-    <Box sx={{ bgcolor: "background.secondary" }}>
+    <Paper sx={{ bgcolor: "background.secondary" }}>
         <Typography variant="h4" className="border-b-accent">Einloggen</Typography>
-      <form action="">
-        <FormControl>
-          <FormLabel htmlFor="email">Email</FormLabel>
-          <TextField
-            type="email"
-            variant="filled"
-            placeholder="beispiel@mail.com"
-            color="accent"
-          />
-          <FormLabel htmlFor="password">Passwort</FormLabel>
-          <FilledInput
-            type={showPassword ? "text" : "password"}
-            placeholder="Passwort"
-            color="accent"
-            endAdornment={
-              <IconButton onClick={setShowPassword}>
-                {showPassword ? (
-                  <VisibilityOff sx={{ color: "text.primary" }} />
-                ) : (
-                  <Visibility sx={{ color: "text.primary" }} />
-                )}
-              </IconButton>
-            }
-          />
-          <CallToAction text="Einloggen" />
-        </FormControl>
-      </form>
-    </Box>
+    </Paper>
   );
 }
