ab99bc5a052aae10a0df6bb489f8f9fad87bac7f
phil@home <philipp.millner@outlook.com>
2024-04-05 20:19:42 +0200

    [new] implemented ForgotPassword page with email form

--- client/src/routes/auth/pages/ForgotPassword.tsx
+++ client/src/routes/auth/pages/ForgotPassword.tsx
@@ -1,6 +1,151 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Backdrop, CircularProgress, Link, Paper, Stack, Typography } from "@mui/material";
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { BREAKPOINTS } from "../../../configs/Breakpoints";
+import { URLS } from "../../../configs/Links";
+import AlertSnackbar from "../../../shared/components/AlertSnackbar";
+import CallToAction from "../../../shared/components/buttons/CallToAction";
+import InputText from "../../../shared/components/form/InputText";
+import { WindowContext } from "../../../shared/context/ScreenSize.context";
+import { useAuthFetch } from "../../../shared/hooks/api/useAuthFetch.hook";
+import useToggle from "../../../shared/hooks/useToggle.hook";
+import { EmailSchema } from "../schemas/Email.schema";
+import SuccessScreen from "./SuccessScreen";
+
+interface IResetFormInput {
+  email: string;
+}
+
+const defaultValues: IResetFormInput = {
+  email: "",
+};
+
+function getSnackbarMessage(error: string) {
+  if (error.includes("Too Many Requests"))
+    return "Die Verifizierungsmail wurde bereits gesendet. Bitte überprüfe deinen Posteingang.";
+
+  if (error.includes("User not found"))
+    return "Der Benutzer wurde nicht gefunden. Bitte registriere dich erneut.";
+
+  return "Es ist ein Fehler aufgetreten. Bitte versuche es später erneut.";
+}
 
 export default function ForgotPassword() {
+  const [wasSuccessful, setWasSuccessful] = useState(false);
+  const [isSnackbarOpen, toggleSnackbarOpen] = useToggle(false);
+  const { width } = useContext(WindowContext);
+  const [snackBarProps, setSnackbarProps] = useState<{
+    message: string;
+    alertType: "success" | "error";
+  }>({
+    message: "",
+    alertType: "success",
+  });
+  const { control, handleSubmit, reset } = useForm<IResetFormInput>({
+    defaultValues,
+    resolver: zodResolver(EmailSchema),
+  });
+
+  const handleError = ({ message }: Error) => {
+    setSnackbarProps({ message: getSnackbarMessage(message), alertType: "error" });
+    toggleSnackbarOpen();
+  };
+
+  const handleSuccess = () => setWasSuccessful(true);
+
+  const { mutate, isPending } = useAuthFetch(
+    handleSuccess,
+    handleError,
+    URLS.API_ENDPOINTS.AUTH.FORGOT_PASSWORD
+  );
+
+  const onSubmit = (data: IResetFormInput) => {
+    data.email = data.email.toLowerCase();
+    mutate(data);
+    reset(defaultValues);
+  };
+
+  const stackStyles = {
+    width: "100%",
+    maxWidth: BREAKPOINTS.md,
+    alignItems: "center",
+    justifyContent: "center",
+    mt: { xs: 20, md: 0 },
+  };
+
+  const paperStyles = {
+    p: 4,
+    my: 5,
+    bgcolor: "background.secondary",
+    borderRadius: ".375rem",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 2,
+    width: "100%",
+  };
+
+  if (wasSuccessful) {
     return (
-        <h1>ForgotPassword</h1>
+      <SuccessScreen
+        title="E-Mail für die Passwortwiederherstellung gesendet"
+        text="Bitte überprüfe deinen Posteingang, um dein Passwort zurückzusetzen."
+        button={{
+          text: "Zurück zum Login",
+          to: URLS.API_ENDPOINTS.AUTH.SIGNIN,
+        }}
+      />
     );
+  }
+
+  return (
+    <>
+      {isPending && (
+        <Backdrop open={isPending} sx={{ zIndex: 100 }}>
+          <CircularProgress color="primary" />
+        </Backdrop>
+      )}
+      <Stack sx={stackStyles}>
+        <Paper sx={paperStyles}>
+          {width > BREAKPOINTS.md && (
+            <Link href={URLS.HOME} underline="hover" sx={{ alignSelf: "flex-start" }}>
+              <ArrowBackIcon sx={{ fontSize: 30 }} />
+            </Link>
+          )}
+          <Typography variant="h5" align="left" className="border-b-primary">
+            Passwort zurücksetzen
+          </Typography>
+          <Typography variant="body1" align="left">
+            Du hast dein Passwort vergessen? Kein Problem! Gib deine E-Mail-Adresse ein und wir senden dir eine E-Mail mit weiteren Anweisungen. Bitte überprüfe auch deinen Spam-Ordner.
+          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
+            <Stack spacing={2} sx={{ width: "100%" }}>
+              <InputText
+                control={control}
+                name="email"
+                label="E-Mail-Adresse"
+                type="email"
+                placeholder="beispiel@mail.com"
+              />
+              <CallToAction
+                type="submit"
+                text="E-Mail senden"
+                fullWidth
+                onClick={() => handleSubmit(onSubmit)}
+              />
+            </Stack>
+          </form>
+        </Paper>
+      </Stack>
+      <AlertSnackbar
+        alertType={snackBarProps.alertType}
+        message={snackBarProps.message}
+        open={isSnackbarOpen}
+        onClose={toggleSnackbarOpen}
+      />
+    </>
+  );
 }
