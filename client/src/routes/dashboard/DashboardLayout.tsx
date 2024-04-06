a2bc59ecb6adf3006da19443c4b080a6d1c2f71e
phil@home <philipp.millner@outlook.com>
2024-04-06 18:59:36 +0200

    dashboard layout fetches user data

--- client/src/routes/dashboard/DashboardLayout.tsx
+++ client/src/routes/dashboard/DashboardLayout.tsx
@@ -1,13 +1,26 @@
-import { Outlet } from "react-router-dom";
-import ScrollToTop from "../../shared/components/ScrollToTop";
 import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import LoadingScreen from "../../shared/components/LoadingScreen";
+import ScrollToTop from "../../shared/components/ScrollToTop";
+import { useUser } from "../../shared/hooks/api/useUser.hook";
+import { UserContext } from "../../shared/types/User";
 
 export default function DashboardLayout() {
+  const { user, isPending, isError } = useUser();
+
+  if (isPending) return (
+    <LoadingScreen />
+  )
+
+  if (isError) return (
+    <div>Something went wrong...</div>
+  )
+
   return (
     <>
       <ScrollToTop />
       <Box>
-        <Outlet />
+        <Outlet context={{ user } satisfies UserContext}/>
       </Box>
     </>
   );
--- client/src/routes/dashboard/pages/Root.tsx
+++ client/src/routes/dashboard/pages/Root.tsx
@@ -1,8 +1,15 @@
+import { useOutletContext } from "react-router-dom";
+import { UserContext } from "../../../shared/types/User";
 
 export default function DashboardRoot() {
+
+    const { user } = useOutletContext<UserContext>();
+    console.log(user);
+    const { first_name, last_name } = user;
+
     return (
         <div>
-        <h1>Dashboard</h1>
+            <h1>Welcome {first_name} {last_name}!</h1>
         </div>
     );
 }
