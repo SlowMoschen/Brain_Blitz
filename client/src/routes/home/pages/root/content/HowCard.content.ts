633b94ffd4a884b9b1e647ab2a639c6a2a0aa9e5
phil@home <philipp.millner@outlook.com>
2024-04-07 15:42:23 +0200

    removed redunant code - implemented to new internal components
    One which renders the steps in a Stack and the other renders it in a grid
    Implemented one source of truth for the content in the cards

---
+++ client/src/routes/home/pages/root/content/HowCard.content.ts
@@ -0,0 +1,30 @@
+interface HowCardContent {
+  number: number;
+  title: string;
+  description: string;
+}
+
+export const howCardContent: HowCardContent[] = [
+  {
+    number: 1,
+    title: "Registrieren",
+    description: "Regestriere dich, kostenlos und unverbindlich!",
+  },
+  {
+    number: 2,
+    title: "Quiz auswählen",
+    description: "Wähle eines der Start-Quizzes aus, welches dich interessiert!",
+  },
+  {
+    number: 3,
+    title: "Absolviere das Quiz",
+    description:
+      "Beantworte Fragen unter Zeitdruck, sammle Punkte für jede richtige Antwort und sei clever, denn falsche Antworten kosten dich wertvolle Punkte. Aber keine Sorge, die Zeit arbeitet für dich! Je schneller du bist, desto mehr Punkte kannst du zusätzlich verdienen.",
+  },
+  {
+    number: 4,
+    title: "Neue Kategorien Freischalten",
+    description:
+      "Nach erfolgreichem Absolvieren eines Quiz schaltest du neue faszinierende Quizzes in verschiedenen Kategorien frei, die darauf warten, von dir entdeckt zu werden.",
+  },
+];
