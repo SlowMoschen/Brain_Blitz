b5585a36de27c77c9af06fcd2105c313bd03274e
phil@home <philipp.millner@outlook.com>
2024-04-07 15:11:17 +0200

    replaced box with stack component

--- client/src/routes/home/RootLayout.tsx
+++ client/src/routes/home/RootLayout.tsx
@@ -1,12 +1,12 @@
-import { Box } from "@mui/material";
+import { Stack } from "@mui/material";
 import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import { BREAKPOINTS } from "../../configs/Breakpoints";
 import ScrollToTop from "../../shared/components/ScrollToTop";
 import { WindowContext } from "../../shared/context/ScreenSize.context";
+import Footer from "./components/footer/Footer";
 import MobileNavbar from "./components/navbar/_MobileNavbar.main";
 import Navbar from "./components/navbar/_Navbar.main";
-import Footer from "./components/footer/Footer";
 
 export default function RootLayout() {
   const { width } = useContext(WindowContext);
@@ -15,19 +15,9 @@ export default function RootLayout() {
     <>
       <ScrollToTop />
       {width > BREAKPOINTS.md ? <Navbar /> : <MobileNavbar />}
-      <Box
-        sx={{
-          height: "fit-content",
-          width: "100%",
-          position: "relative",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <Stack alignItems={'center'} width={'100%'} position={'relative'}>
         <Outlet />
-      </Box>
+      </Stack>
       <Footer />
     </>
   );
