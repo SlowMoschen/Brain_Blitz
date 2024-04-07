bf99aa86f6751d49557cc7b955d7633344070d15
phil@home <philipp.millner@outlook.com>
2024-04-07 17:01:49 +0200

    added bool value to check if user has access to the dashbord
    To handle users which enters urls to dashboard directly

--- client/src/shared/hooks/api/useUser.hook.tsx
+++ client/src/shared/hooks/api/useUser.hook.tsx
@@ -1,16 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { URLS } from "../../../configs/Links";
 import { HttpService } from "../../services/httpService.service";
+import { User } from "../../types/User";
 
 const httpService = new HttpService(); 
 const getUser = async () => {
     return await httpService.get(URLS.API_ENDPOINTS.APP.USER);
   }
 
-export function useUser() {
-    const { data: res, isPending, isError } = useQuery({ queryKey: ['user'], queryFn: getUser });
+export function useUser(): { user: User, isPending: boolean, isError: boolean, noAccess: boolean } {
+    const { data: res, isPending, isError, error, } = useQuery({ queryKey: ['user'], queryFn: getUser });
 
-    const user = res?.data;
+    const user: User = res?.data;
+    const noAccess: boolean = error?.message.includes('Forbidden') || error?.message.includes('Unauthorized') ? true : false;
 
-    return { user, isPending, isError };
+    return { user, isPending, isError, noAccess };
 }
