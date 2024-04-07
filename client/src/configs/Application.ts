f46c461f28b1abb68eede9c2f14fe871ee7704a4
phil@home <philipp.millner@outlook.com>
2024-04-08 01:45:31 +0200

    added a interval used by useUserFetch hook

--- client/src/configs/Application.ts
+++ client/src/configs/Application.ts
@@ -4,3 +4,5 @@ export const TIMES = {
     SNACKBAR_DELAY: 4000,
     ERROR_MESSAGE_DURATION: 5000,
 }
+
+export const USER_REFRESH_INTERVAL = (15 - (new Date().getMinutes() % 15) + 1) * 60 * 1000; // on every .15th minute of the hour
