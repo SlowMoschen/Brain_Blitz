4ede399535c7acf6c9f147faed556064a04003a8
phil@home <philipp.millner@outlook.com>
2024-04-08 01:46:21 +0200

    implemented an interval to refetch user on every .15 Minute of the hour
    mainly to keep energy level up to date

--- client/src/shared/hooks/api/useUserFetch.hook.tsx
+++ client/src/shared/hooks/api/useUserFetch.hook.tsx
@@ -2,17 +2,37 @@ import { useQuery } from "@tanstack/react-query";
 import { URLS } from "../../../configs/Links";
 import { HttpService } from "../../services/httpService.service";
 import { User } from "../../types/User";
+import { USER_REFRESH_INTERVAL } from "../../../configs/Application";
 
-const httpService = new HttpService(); 
+const httpService = new HttpService();
 const getUser = async () => {
-    return await httpService.get(URLS.API_ENDPOINTS.APP.USER);
-  }
+  return await httpService.get(URLS.API_ENDPOINTS.APP.USER);
+};
 
-export function useUserFetch(): { user: User, isPending: boolean, isError: boolean, noAccess: boolean } {
-    const { data: res, isPending, isError, error, } = useQuery({ queryKey: ['user'], queryFn: getUser });
+/**
+ * @description Fetches user data from the server - will be refetched on every .15th minute of the hour to keep the data up-to-date.
+ * @returns {Object} with following properties:
+ * - user: User
+ * - isPending: boolean
+ * - isError: boolean
+ * - noAccess: boolean
+ */
+export function useUserFetch(): {
+  user: User;
+  isPending: boolean;
+  isError: boolean;
+  noAccess: boolean;
+} {
+  const {
+    data: res,
+    isPending,
+    isError,
+    error,
+  } = useQuery({ queryKey: ["user"], queryFn: getUser, refetchInterval: USER_REFRESH_INTERVAL });
 
-    const user: User = res?.data;
-    const noAccess: boolean = error?.message.includes('Forbidden') || error?.message.includes('Unauthorized') ? true : false;
+  const user: User = res?.data;
+  const noAccess: boolean =
+    error?.message.includes("Forbidden") || error?.message.includes("Unauthorized") ? true : false;
 
-    return { user, isPending, isError, noAccess };
+  return { user, isPending, isError, noAccess };
 }
