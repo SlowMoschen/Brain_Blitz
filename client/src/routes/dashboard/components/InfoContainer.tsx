e63037f555ae47d989dc600218306dc410a38f5d
phil@home <philipp.millner@outlook.com>
2024-04-08 13:15:12 +0200

    [new] Container to display one infor in one line - used by Login and Energy Containers

---
+++ client/src/routes/dashboard/components/InfoContainer.tsx
@@ -0,0 +1,21 @@
+import { Stack } from "@mui/material";
+
+interface InfoContainerProps {
+  children: React.ReactNode;
+}
+
+export default function InfoContainer({ children }: InfoContainerProps) {
+  const container = {
+    bgcolor: "primary.light",
+    borderRadius: ".375rem",
+    height: "50px",
+    width: '90%',
+    my: 1,
+  };
+
+  return (
+    <Stack direction={"row"} justifyContent={"space-evenly"} alignItems={"center"} sx={container}>
+      {children}
+    </Stack>
+  );
+}
