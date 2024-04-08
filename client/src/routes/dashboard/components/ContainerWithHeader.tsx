1f7e25a570f244e492bae1c5a1f60123bfb367b2
phil@home <philipp.millner@outlook.com>
2024-04-08 13:13:29 +0200

    [new] container with a header in accent light color

---
+++ client/src/routes/dashboard/components/ContainerWithHeader.tsx
@@ -0,0 +1,30 @@
+import { Box, Typography } from "@mui/material";
+
+interface ContainerWithHeaderProps {
+    header: string;
+    children: React.ReactNode;
+    sx?: { [key: string]: string | number };
+}
+
+export default function ContainerWithHeader({ header, children, sx }: ContainerWithHeaderProps) {
+
+    const mainContainer = {
+        bgcolor: "background.secondary",
+        borderRadius: '.375rem',
+        ...sx,
+    }
+    
+    const headerContainer = {
+        bgcolor: 'accent.light',
+        p: 2,
+    }
+
+    return (
+        <Box sx={mainContainer}>
+            <Box sx={headerContainer}>
+                <Typography variant="h4">{header}</Typography>
+            </Box>
+            {children}
+        </Box>
+    );
+}
