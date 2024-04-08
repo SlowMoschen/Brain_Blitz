3c1de661ea6e699f6b9ed155b947465047647a1b
phil@home <philipp.millner@outlook.com>
2024-04-08 13:14:45 +0200

    [new] Info for the user what its login streak is

---
+++ client/src/routes/dashboard/components/LoginStreak.tsx
@@ -0,0 +1,22 @@
+import { Typography } from "@mui/material";
+import InfoContainer from "./InfoContainer";
+import StarIcon from '@mui/icons-material/Star';
+
+interface LoginStreakProps {
+  streak: number;
+}
+
+export default function LoginStreak({ streak = 0 }: LoginStreakProps) {
+  return (
+    <>
+      <InfoContainer>
+        <StarIcon sx={{ color: "yellow" }} />
+        <Typography variant="caption">Login-Streak:</Typography>
+        <Typography variant="h6" color={"accent.main"}>
+          {streak}
+        </Typography>
+        <Typography variant="caption">Tage</Typography>
+      </InfoContainer>
+    </>
+  );
+}
