f971a062bbc9802ac95da08a5f74add0e60e5e37
phil@home <philipp.millner@outlook.com>
2024-04-08 13:14:03 +0200

    [new] Components tracks energy level and the time to the next quater hour

---
+++ client/src/routes/dashboard/components/EnergyTracker.tsx
@@ -0,0 +1,26 @@
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import { Typography } from "@mui/material";
+import use15MinuteTimer from '../../../shared/hooks/use15MinuteTimer.hook';
+import InfoContainer from "./InfoContainer";
+
+interface EnergyTrackerProps {
+    energy: number;
+    }
+
+export default function EnergyTracker({ energy = 100 }: EnergyTrackerProps) {
+    const { time } = use15MinuteTimer();
+
+
+    return (
+        <>
+        <InfoContainer>
+          <FlashOnIcon sx={{ color: "yellow" }} />
+          <Typography variant="caption">Energie:</Typography>
+          <Typography variant="h6" color={"accent.main"}>
+            {energy}
+          </Typography>
+            <Typography variant="caption">{time}</Typography>
+        </InfoContainer>
+      </>
+    )
+}
