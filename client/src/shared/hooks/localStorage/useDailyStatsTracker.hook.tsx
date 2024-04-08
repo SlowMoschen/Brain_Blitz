3ab63839493f29bb75a375c97c243496d0b3aca5
phil@home <philipp.millner@outlook.com>
2024-04-08 17:29:21 +0200

    implemented a hook to set and get DailyStats - uses localStorage hook

---
+++ client/src/shared/hooks/localStorage/useDailyStatsTracker.hook.tsx
@@ -0,0 +1,27 @@
+import { useLocalStorage } from "./useLocalStorage.hook";
+
+export interface IDailyStats {
+    playedQuizzes: number;
+    points: number;
+    answeredQuestions: number;
+    timePlayed: string;
+}
+
+export function useDailyStatsTracker() {
+    const { setData, getData, removeData } = useLocalStorage();
+    const key = "brain-blitz-daily-stats";
+
+    const getDailyStats = (): IDailyStats | null => {
+        return getData(key);
+    }
+
+    const updateDailyStats = (stats: IDailyStats) => {
+        setData(key, stats);
+    }
+
+    const resetDailyStats = () => {
+        removeData(key);
+    }
+
+    return { getDailyStats, updateDailyStats, resetDailyStats };
+}
