a396c3012aa25ee311c06a05c7eb410f37d74ff8
phil@home <philipp.millner@outlook.com>
2024-04-10 01:56:15 +0200

    added game configs

--- client/src/configs/Application.ts
+++ client/src/configs/Application.ts
@@ -7,6 +7,15 @@ export const TIMES = {
 
 export const ENERGY_CONSUPMTION = 15;
 
+export const GAME = {
+    ENERGY_CONSUPMTION: 15,
+    ENERGY_RECHARGE: 10,
+    CORRECT_ANSWER_POINTS: 500,
+    WRONG_ANSWER_POINTS: 300,
+    POINTS_PER_SECOND: 10,
+    QUIZ_COMPLETE_THRESHOLD: 1000,
+    TIME_PER_QUIZ: 3 * 60 * 1000, // 3 minutes
+}
 /**
  * @description A function to calculate the time until the next quarter hour.
  * @returns {number} - The time in milliseconds until the next quarter hour.
