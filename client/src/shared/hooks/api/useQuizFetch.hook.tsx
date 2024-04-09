d66693c09e8f7d6a54b3e5db6ec494b86a3cdb39
phil@home <philipp.millner@outlook.com>
2024-04-10 00:34:55 +0200

    disabled refetch on window focus and only retrying only once

--- client/src/shared/hooks/api/useQuizFetch.hook.tsx
+++ client/src/shared/hooks/api/useQuizFetch.hook.tsx
@@ -5,9 +5,12 @@ import { URLS } from "../../../configs/Links";
 const httpService = new HttpService();
 
 export function useQuizFetch(quizID: string) {
+
   const { isPending, data, error, isError } = useQuery({
     queryKey: ["quiz-start"],
-    queryFn: () => httpService.get(URLS.API_ENDPOINTS.APP.QUIZ_START + quizID),
+    queryFn: () => httpService.get(URLS.API_ENDPOINTS.APP.QUIZ_START + quizID), 
+    retry: 1,
+    refetchOnWindowFocus: false,
   });
 
   const quizData = data?.data;
