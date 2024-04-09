0ffc453d411b71859fd7b982e0add43b39d2cfaa
phil@home <philipp.millner@outlook.com>
2024-04-09 12:55:12 +0200

    implemented a hook to query quiz data for playing a quiz

---
+++ client/src/shared/hooks/api/useQuizFetch.hook.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { HttpService } from "../../services/httpService.service";
+import { URLS } from "../../../configs/Links";
+
+const httpService = new HttpService();
+
+export function useQuizFetch(quizID: string) {
+  const { isPending, data, error, isError } = useQuery({
+    queryKey: ["quiz-start"],
+    queryFn: () => httpService.get(URLS.API_ENDPOINTS.APP.QUIZ_START + quizID),
+  });
+
+  const quizData = data?.data;
+
+    return { isPending, quizData, error, isError };
+}
