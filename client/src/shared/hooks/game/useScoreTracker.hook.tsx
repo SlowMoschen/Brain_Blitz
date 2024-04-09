45536aa0bc2563a0f6d7e30860f35a3606dae217
phil@home <philipp.millner@outlook.com>
2024-04-10 01:53:42 +0200

    implemented a scoreTracker hook
    * Can increment, decrement score with set Values
    * can calculate the timeBonus score
    * returns the current score

---
+++ client/src/shared/hooks/game/useScoreTracker.hook.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { GAME } from "../../../configs/Application";
+
+/**
+ * @description A custom hook to track the score of the user while playing the quiz.
+ * @returns {Object} - An object containing the current score, a function to increase the score, a function to decrease the score and a function to calculate the time bonus.
+ * @returns {number} score - The current score of the user.
+ * @returns {function} increaseScore - A function to increase the score.
+ * @returns {function} decreaseScore - A function to decrease the score.
+ * @returns {function} calculateTimeBonus - A function to calculate the time bonus.
+ */
+export function useScoreTracker() {
+    const [currentScore, setCurrentScore] = useState<number>(0);
+
+    const increaseScore = () => {
+        setCurrentScore((prev: number) => prev + GAME.CORRECT_ANSWER_POINTS);
+    };
+
+    const decreaseScore = () => {
+        setCurrentScore((prev: number) => prev - GAME.WRONG_ANSWER_POINTS);
+    };
+
+    /**
+     * @description A function to calculate the time bonus.
+     * @param {number} time - The time in SECONDS.
+     * @returns {void}
+     */
+    const calculateTimeBonus = (time: number) => {
+        setCurrentScore((prev: number) => prev + time * GAME.POINTS_PER_SECOND);
+    }
+
+    return { currentScore, increaseScore, decreaseScore, calculateTimeBonus };
+}
