fd53a88bb5ffcdacfb2dc74157ce29513fd3099d
phil@home <philipp.millner@outlook.com>
2024-04-10 17:41:38 +0200

    Endscreen when a quiz ended

---
+++ client/src/routes/quiz/QuizEndScreen.tsx
@@ -0,0 +1,44 @@
+import CancelIcon from '@mui/icons-material/Cancel';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import { Stack, Typography } from "@mui/material";
+import { GAME } from "../../configs/Application";
+import { URLS } from "../../configs/Links";
+import RouterButton from "../../shared/components/buttons/RouterButton";
+import { useTimeParser } from "../../shared/hooks/timer/useTimeParser.hook";
+import ScoreTable from "./components/ScoreTable";
+
+interface QuizEndScreenProps {
+    answersCount: {
+        correct: number;
+        incorrect: number;
+    };
+    time: number;
+    isSuccess: boolean;
+}
+
+export default function QuizEndScreen({ answersCount, time, isSuccess }: QuizEndScreenProps) {
+    const { parseMinuteString } = useTimeParser();
+    const timeUsed = parseMinuteString(GAME.TIME_PER_QUIZ - time);
+
+    const correctPoints = answersCount.correct * GAME.CORRECT_ANSWER_POINTS;
+    const incorrectPoints = answersCount.incorrect * GAME.WRONG_ANSWER_POINTS;
+    const timeBonus = (time / 1000) * GAME.POINTS_PER_SECOND;
+
+    return (
+        <>
+            <Stack height={'90vh'} justifyContent={'center'}>
+                <Stack alignItems={'center'}>
+                    {isSuccess ? <VerifiedIcon sx={{ color: 'accent.main', fontSize: 40 }} /> : <CancelIcon sx={{ color: 'primary.main', fontSize: 40 }} />}
+                    <Typography variant="h4">
+                        {isSuccess ? "Abgeschlossen" : "Nicht geschafft"}
+                    </Typography>
+                    <Typography variant="subtitle1">
+                        Zeit: {timeUsed}
+                    </Typography>
+                </Stack>
+                <ScoreTable correctPoints={correctPoints} incorrectPoints={incorrectPoints} timeBonus={timeBonus} />
+                <RouterButton to={URLS.DASHBOARD} variant="contained" color="primary" text="Zurück zum Dasboard" />
+            </Stack>
+        </>
+    )
+}
