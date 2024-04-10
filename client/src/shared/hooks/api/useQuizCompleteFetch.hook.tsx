e930d97a685fa486a87e6a174343f23d7dd23d52
phil@home <philipp.millner@outlook.com>
2024-04-10 19:32:18 +0200

    implemented a hook to send completed quiz data

---
+++ client/src/shared/hooks/api/useQuizCompleteFetch.hook.tsx
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { HttpService } from "../../services/httpService.service";
+import { URLS } from "../../../configs/Links";
+const httpService = new HttpService();
+
+interface QuizCompleteDTO {
+  correct_answers: number;
+  incorrect_answers: number;
+  score: number;
+}
+
+function fetchQuizComplete(quizID: string, body: QuizCompleteDTO) {
+    return httpService.post(URLS.API_ENDPOINTS.APP.QUIZ_COMPLETE + quizID, body);
+}
+
+export function useQuizCompleteFetch(quizID: string) {
+        const queryClient = useQueryClient();
+
+    const { isPending, data, error, isError, mutate } = useMutation({
+        mutationFn: (body: QuizCompleteDTO) => fetchQuizComplete(quizID, body),
+        onSuccess: () => {
+                queryClient.invalidateQueries({ queryKey: ['user'] });
+        }
+    });
+
+    return { isPending, data, error, isError, mutate };
+}
