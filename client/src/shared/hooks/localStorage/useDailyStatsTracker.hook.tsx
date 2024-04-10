37ffb6c9b7a85c40e9c9dd181416ec060ba36fc6
phil@home <philipp.millner@outlook.com>
2024-04-10 19:29:16 +0200

    added a the userId to the key to  retriev the correct stats

--- client/src/shared/hooks/localStorage/useDailyStatsTracker.hook.tsx
+++ client/src/shared/hooks/localStorage/useDailyStatsTracker.hook.tsx
@@ -2,62 +2,62 @@ import { useEffect, useState } from "react";
 import { useLocalStorage } from "./useLocalStorage.hook";
 
 export interface IDailyStats {
-    [key: string]: number | string;
-    playedQuizzes: number;
-    points: number;
-    answeredQuestions: number;
-    timePlayed: number;
-    date: string;
+  [key: string]: number | string;
+  playedQuizzes: number;
+  points: number;
+  answeredQuestions: number;
+  timePlayed: number;
+  date: string;
 }
 
 const initialStats: IDailyStats = {
-    playedQuizzes: 0,
-    points: 0,
-    answeredQuestions: 0,
-    timePlayed: 0,
-    date: new Date().toDateString(),
-}
+  playedQuizzes: 0,
+  points: 0,
+  answeredQuestions: 0,
+  timePlayed: 0,
+  date: new Date().toDateString(),
+};
 
 export function useDailyStatsTracker() {
-    const [dailyStats, setDailyStats] = useState<IDailyStats>(initialStats);
-    const { setData, getData, removeData } = useLocalStorage();
-    const key = `dailyStats-${getData('brain-blitz-user-id')}`;
+  const [dailyStats, setDailyStats] = useState<IDailyStats>(initialStats);
+  const { setData, getData, removeData } = useLocalStorage();
+  const key = `dailyStats-${getData("brain-blitz-user-id")}`;
 
-    const getDailyStats = (): IDailyStats | null => {
-        return getData(key);
+  const getDailyStats = (): IDailyStats | null => {
+    return getData(key);
+  };
+
+  const updateDailyStats = (stats: IDailyStats) => {
+    const currentStats = getDailyStats();
+    if (!currentStats) return setData(key, stats);
+
+    const updatedStats: IDailyStats = {
+      playedQuizzes: currentStats.playedQuizzes + stats.playedQuizzes,
+      points: currentStats.points + stats.points,
+      answeredQuestions: currentStats.answeredQuestions + stats.answeredQuestions,
+      timePlayed: currentStats.timePlayed + stats.timePlayed,
+      date: currentStats.date,
+    };
+
+    setData(key, updatedStats);
+  };
+
+  const resetDailyStats = () => {
+    removeData(key);
+  };
+
+  useEffect(() => {
+    const stats = getDailyStats();
+
+    if (!stats) return updateDailyStats(initialStats);
+
+    if (stats.date !== new Date().toDateString()) {
+      resetDailyStats();
+      return updateDailyStats(initialStats);
     }
 
-    const updateDailyStats = (stats: IDailyStats) => {
-        const currentStats = getDailyStats();
-        if (!currentStats) return setData(key, stats);
+    setDailyStats(stats);
+  }, []);
 
-       const updatedStats: IDailyStats = {
-            playedQuizzes: currentStats.playedQuizzes + stats.playedQuizzes,
-            points: currentStats.points + stats.points,
-            answeredQuestions: currentStats.answeredQuestions + stats.answeredQuestions,
-            timePlayed: currentStats.timePlayed + stats.timePlayed,
-            date: currentStats.date,
-       }
-
-         setData(key, updatedStats);
-    }
-
-    const resetDailyStats = () => {
-        removeData(key);
-    }
-
-    useEffect(() => {
-        const stats = getDailyStats();
-        
-        if (!stats) return updateDailyStats(initialStats);
-
-        if (stats.date !== new Date().toDateString()) {
-            resetDailyStats();
-            return updateDailyStats(initialStats);
-        }
-
-        setDailyStats(stats);
-    }, []);
-
-    return { getDailyStats, updateDailyStats, resetDailyStats, dailyStats };
+  return { getDailyStats, updateDailyStats, resetDailyStats, dailyStats };
 }
