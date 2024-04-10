e10566a66db2317126c6cb587ccc712b187eacfb
phil@home <philipp.millner@outlook.com>
2024-04-10 17:36:11 +0200

    type renames

--- client/src/shared/hooks/useCompareUserQuizzes.tsx
+++ client/src/shared/hooks/useCompareUserQuizzes.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useRef } from "react";
-import { User } from "../types/User";
+import { IUser } from "../types/User";
 
-export function useCompareUserQuizzes(newUser: User) {
-    const user = useRef<User | null>(null);
+export function useCompareUserQuizzes(newUser: IUser) {
+    const user = useRef<IUser | null>(null);
 
-  const compareQuizzes = (newUser: User) => {
+  const compareQuizzes = (newUser: IUser) => {
     if (user.current) {
       const newUnlockedQuizzes = newUser.unlocked_quizzes.map((table) => table.quiz);
       const oldUnlockedQuizzes = user.current.unlocked_quizzes.map((table) => table.quiz);
