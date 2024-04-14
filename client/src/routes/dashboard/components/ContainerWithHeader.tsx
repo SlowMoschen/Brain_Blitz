36ed32f2f3f3012cc9fc97ec67229d402d9235c9
phil@home <philipp.millner@outlook.com>
2024-04-14 15:40:57 +0200

    added an optional caption

--- client/src/routes/dashboard/components/ContainerWithHeader.tsx
+++ client/src/routes/dashboard/components/ContainerWithHeader.tsx
@@ -2,12 +2,13 @@ import { Box, Typography } from "@mui/material";
 
 interface ContainerWithHeaderProps {
   header: string;
+  caption?: string;
   children: React.ReactNode;
   sx?: { [key: string]: string | number };
   center?: boolean;
 }
 
-export default function ContainerWithHeader({ header, children, sx, center }: ContainerWithHeaderProps) {
+export default function ContainerWithHeader({ header, children, sx, center, caption }: ContainerWithHeaderProps) {
   const mainContainer = {
     bgcolor: "background.secondary",
     borderRadius: ".375rem",
@@ -19,12 +20,18 @@ export default function ContainerWithHeader({ header, children, sx, center }: Co
     borderRadius: ".375rem .375rem 0 0",
     bgcolor: "accent.light",
     p: 2,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
   };
 
   return (
     <Box sx={mainContainer}>
       <Box sx={headerContainer}>
         <Typography variant="h5" textAlign={center ? 'center' : 'left'}>{header}</Typography>
+        <Typography variant="caption" textAlign={center ? 'center' : 'left'}>
+          {caption}
+        </Typography>
       </Box>
       {children}
     </Box>
