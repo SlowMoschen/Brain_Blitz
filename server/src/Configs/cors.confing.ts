98f3b9029165f678985d3a5dd7166f0669bfe664
phil@home <philipp.millner@outlook.com>
2024-04-16 19:51:58 +0200

    Update CORS configuration to allow requests from https://brain-blitz.com/

--- server/src/Configs/cors.confing.ts
+++ server/src/Configs/cors.confing.ts
@@ -1,6 +1,6 @@
 import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 export const corsOptions: CorsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://brain-blitz.com/'],
     credentials: true,
 } 
--- server/src/Configs/drizzle.config.ts
+++ server/src/Configs/drizzle.config.ts
@@ -7,4 +7,5 @@ export default {
 	dbCredentials: {
 		connectionString: process.env.DATABASE_URL,
 	},
+	verbose: true,
 } satisfies Config;
