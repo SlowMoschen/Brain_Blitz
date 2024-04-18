78a91f3686c4522b08eca0ff9af9ce07d8704576
phil@home <philipp.millner@outlook.com>
2024-04-18 16:37:54 +0200

    Refactor QuizRanking component to use reusable RankingTable in Quiz.Ranking.tsx

--- client/src/routes/dashboard/pages/rankings/Quiz.Ranking.tsx
+++ client/src/routes/dashboard/pages/rankings/Quiz.Ranking.tsx
@@ -1,16 +1,29 @@
 import { useParams } from "react-router-dom";
 import { useRankingQueries } from "../../../../shared/hooks/api/useRankingQueries.hook";
 import HeaderMenu from "../../components/navigation/HeaderMenu";
-import QuizRankingTable from "./components/QuizRankingTable";
+import RankingTable from "./components/RankingTable";
 
 export default function QuizRanking() {
   const { quizID } = useParams<{ quizID: string }>();
   const { singleQuizRankings } = useRankingQueries().useSingleQuizRankings(quizID!);
+  const tableHeader = singleQuizRankings?.[0]?.quiz_name;
+
+  const tableData = singleQuizRankings?.map((ranking) => ({
+    user_id: ranking.user_id,
+    name: ranking.first_name,
+    value: ranking.points,
+    additionalInfo: ranking.created_at,
+  }));
 
   return (
     <>
       <HeaderMenu />
-      <QuizRankingTable data={singleQuizRankings} />
+      <RankingTable
+        data={tableData}
+        tableHeader={tableHeader}
+        valueString="Punkte"
+        additionalInfoString="Erspielt"
+      />
     </>
   );
 }
