20872f01e6c809718be927e684b2ff6745e25611
phil@home <philipp.millner@outlook.com>
2024-04-19 23:34:13 +0200

    Refactor server deployment path in GitHub Actions workflow and update backend dependencies

--- .github/workflows/deploy_backend.sh
+++ .github/workflows/deploy_backend.sh
@@ -1,22 +1,22 @@
 echo "Change to App directory"
 cd ~/Brain_Blitz/
 
+echo 'kill all pm2 processes'
+sudo pm2 kill
+
 echo "bring repo up to date"
-git pull
+sudo git pull
 
 echo "change to backend directory"
 cd ./server
 
 echo "install dependencies"
-npm install
+sudo npm install
 
 echo "build the app"
-npm run build
-
-echo 'kill all pm2 processes'
-pm2 kill
+sudo npm run build
 
 echo 'start the app'
-pm2 start npm --name "BB" -- start
+sudo pm2 start npm --name "BB" -- start
 
 echo "App Deployed"
