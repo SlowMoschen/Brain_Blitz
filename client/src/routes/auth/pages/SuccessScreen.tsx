df8510d5b70bf6b7537933df6d26098e2e85dbab
phil@home <philipp.millner@outlook.com>
2024-04-19 22:00:10 +0200

    Refactor SuccessScreen component to include caption prop

--- client/src/routes/auth/pages/SuccessScreen.tsx
+++ client/src/routes/auth/pages/SuccessScreen.tsx
@@ -1,16 +1,17 @@
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import RouterButton from "../../../shared/components/buttons/RouterButton";
 
 interface SuccessScreenProps {
   title: string;
   text: string;
+  caption?: string;
   button?: {
     text: string;
     to: string;
   };
 }
 
-export default function SuccessScreen({ title, text, button }: SuccessScreenProps) {
+export default function SuccessScreen({ title, text, button, caption }: SuccessScreenProps) {
   return (
     <>
       <Typography variant="h4" align="center" className="border-b-accent" my={3}>
@@ -19,12 +20,20 @@ export default function SuccessScreen({ title, text, button }: SuccessScreenProp
       <Typography variant="body1" align="center">
         {text}
       </Typography>
-        {button && (
-            <Box mt={2}>
-                <RouterButton to={button.to} variant="outlined" color="primary" text={button.text} />
-
-            </Box>
-        )}
+      {caption && (
+        <Typography variant="body2" align="center" mt={2} sx={{ opacity: .5 }}>
+          {caption}
+        </Typography>
+      )}
+      {button && (
+        <RouterButton
+          to={button.to}
+          variant="outlined"
+          color="primary"
+          text={button.text}
+          sx={{ mt: 2 }}
+        />
+      )}
     </>
   );
 }
