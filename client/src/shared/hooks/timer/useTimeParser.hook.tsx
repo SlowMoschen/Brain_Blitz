1f31727bd7ea8d8a4b56c4b3c29cf5b516e81b6f
phil@home <philipp.millner@outlook.com>
2024-04-29 10:27:24 +0200

    refac:  removed unnecessary functions - moved both timeString parser functions into one

--- client/src/shared/hooks/timer/useTimeParser.hook.tsx
+++ client/src/shared/hooks/timer/useTimeParser.hook.tsx
@@ -6,47 +6,20 @@
 export function useTimeParser() {
     
     /**
-     * @description A function to parse the time in milliseconds to a string in the format of "mm:ss".
+     * @description A function to parse the time in milliseconds to a string in the format "mm:ss" or "hh:mm:ss" if the time is above one hour.
      * @param {number} time - The time in milliseconds.
-     * @returns {string} - The time string in the format "mm:ss".
+     * @returns {string} - The time in the format "mm:ss" or "hh:mm:ss".
      */
     const parseToTimeString = (time: number) => {
         const isAboveOneHour = time >= 3600000;
+        const hours = isAboveOneHour ? Math.floor(time / 3600000) : 0;
         const minutes = Math.floor(time / 60000);
         const seconds = Math.floor((time % 60000) / 1000);
-        return isAboveOneHour
-        ? parseHourString(time)
-        : `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+        if (isAboveOneHour) return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+        return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
     }
 
-    /**
-     * @description A function to parse the time in milliseconds to a string in the format of "hh:mm:ss".
-     * @param {number} time - The time in milliseconds.
-     * @returns {string} - The time string in the format "hh:mm:ss".
-     * @example
-     */
-    const parseHourString = (time: number) => {
-        const hours = Math.floor(time / 3600000);
-        const minutes = Math.floor((time % 3600000) / 60000);
-        const seconds = Math.floor((time % 60000) / 1000);
-        return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-    }
-
-    const parseMillisecondsToSeconds = (time: number) => {
-        return Math.floor(time / 1000);
-    }
-
-    const parseSecondsToMilliseconds = (time: number) => {
-        return time * 1000;
-    }
-
-    const parseMinutesToMilliseconds = (time: number) => {
-        return time * 60000;
-    }
-
-    const parseHoursToMilliseconds = (time: number) => {
-        return time * 3600000;
-    }
-
-    return { parseToTimeString, parseMillisecondsToSeconds, parseSecondsToMilliseconds, parseMinutesToMilliseconds, parseHoursToMilliseconds };
+    return { parseToTimeString };
 }
