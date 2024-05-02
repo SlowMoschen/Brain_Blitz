645678c7d98f2dad2490fe3f29e1fe9e8fe416d3
phil@home <philipp.millner@outlook.com>
2024-05-03 00:29:18 +0200

    made DataTable in Admindashboard generic

--- client/src/routes/dashboard/pages/admin/pages/QuizzesTable.tsx
+++ client/src/routes/dashboard/pages/admin/pages/QuizzesTable.tsx
@@ -1,19 +1,9 @@
-import { useNavigate, useOutletContext } from "react-router-dom";
-import { AdminOutletContext } from "../AdminDasbordLayout";
-import {
-  TableContainer,
-  Paper,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  Checkbox,
-  TableBody,
-} from "@mui/material";
-import QuizCategoryIcon from "../../../components/quiz/QuizCategoryIcon";
 import { useEffect, useState } from "react";
-import { IQuiz } from "../../../../../shared/types/Quiz";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import { URLS } from "../../../../../configs/Links";
+import { IQuiz } from "../../../../../shared/types/Quiz";
+import { AdminOutletContext } from "../AdminDasbordLayout";
+import DataTable from "./DataTable";
 
 interface QuizzesTableProps {
   category?: string | null;
@@ -21,7 +11,6 @@ interface QuizzesTableProps {
 
 export default function QuizzesTable({ category }: QuizzesTableProps) {
   const redirect = useNavigate();
-  const [selected, setSelected] = useState<string[]>([]);
   const [quizzes, setQuizzes] = useState<IQuiz[]>([]);
   const { allQuizzes } = useOutletContext<AdminOutletContext>();
 
@@ -37,83 +26,20 @@ export default function QuizzesTable({ category }: QuizzesTableProps) {
   }, [allQuizzes, category]);
 
   const columns = [
-    { field: "id", headerName: "ID" },
-    { field: "category", headerName: "Category" },
-    { field: "title", headerName: "Title" },
-    { field: "createdAt", headerName: "Created at" },
-    { field: "updatedAt", headerName: "Updated at" },
+    { key: "category", title: "Kategorie" },
+    { key: "id", title: "ID" },
+    { key: "title", title: "Titel" },
+    { key: "created_at", title: "Erstellt am" },
   ];
 
-  const handleRowClick = (id: string) => {
-    console.log(id);
-    redirect(URLS.ADMIN_ROUTES.QUIZ + `/${id}`)
-  };
-
-  const handleSelectAllClick = () => {
-    if (selected.length === quizzes.length) {
-      setSelected([]);
-    } else {
-      setSelected(quizzes.map((quiz) => quiz.id));
-    }
-  };
-
-  const handleSelectClick = (id: string) => {
-    if (selected.includes(id)) {
-      setSelected(selected.filter((selectedId) => selectedId !== id));
-    } else {
-      setSelected([...selected, id]);
-    }
-  };
-
   return (
-    <TableContainer component={Paper} sx={{ flexGrow: 1, maxWidth: "100vw" }}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Checkbox
-                indeterminate={selected.length > 0 && selected.length < quizzes.length}
-                checked={selected.length === quizzes.length}
-                onChange={handleSelectAllClick}
-              />
-            </TableCell>
-            {columns.map((column) => (
-              <TableCell key={column.field} align="center">
-                {column.headerName}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {quizzes.map((quiz) => (
-            <TableRow
-              key={quiz.id}
-              onClick={() => handleRowClick(quiz.id)}
-              sx={{
-                bgcolor: selected.includes(quiz.id) ? "accent.light" : "inherit",
-                "&:hover": { bgcolor: "background.default", cursor: "pointer" },
-              }}
-            >
-              <TableCell>
-                <Checkbox
-                  checked={selected.includes(quiz.id)}
-                  onChange={() => handleSelectClick(quiz.id)}
-                />
-              </TableCell>
-              <TableCell align="center">{quiz.id}</TableCell>
-              <TableCell align="center">
-                <QuizCategoryIcon
-                  category={quiz.category}
-                  sx={{ borderRadius: 0, fontSize: 40, pt: 1 }}
-                />
-              </TableCell>
-              <TableCell align="center">{quiz.title}</TableCell>
-              <TableCell align="center">{new Date(quiz.created_at).toDateString()}</TableCell>
-              <TableCell align="center">{new Date(quiz.updated_at).toDateString()}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+    <DataTable<IQuiz>
+      columns={columns}
+      rows={quizzes}
+      onRowClick={(e, id) => {
+        if (e.target instanceof HTMLInputElement) return;
+        redirect(URLS.ADMIN_ROUTES.QUIZ + `/${id}`);
+      }}
+    />
+  )
 }
